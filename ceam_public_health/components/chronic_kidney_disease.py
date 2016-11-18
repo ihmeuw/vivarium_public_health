377eb66e72bc4239442e112966cec4926a21cc2c
Alec Deason <alecwd@uw.edu>
2016-11-18 11:08:40 -0800

    very sad typo. Let's pretend that never happened

--- ceam_public_health/components/chronic_kidney_disease.py
+++ ceam_public_health/components/chronic_kidney_disease.py
@@ -5,14 +5,18 @@ from ceam_public_health.components.disease import DiseaseModel, ExcessMortalityS
 
 
 def ckd_factory():
-    component = DiseaseModel('ihd')
+    component = DiseaseModel('ckd')
 
     healthy = State('healthy', key='ckd')
 
-    # TODO: ME 3179 is not the right one to be using for disability weight, figure out which one is
-    stage_three = ExcessMortalityState('stage_three_ckd', disability_weight=get_disability_weight(3179), modelable_entity_id=2018)
-    stage_four = ExcessMortalityState('stage_four_ckd', disability_weight=get_disability_weight(3179), modelable_entity_id=2019)
-    stage_five = ExcessMortalityState('stage_five_ckd', disability_weight=get_disability_weight(3179), modelable_entity_id=2022)
+    # NOTE: According to Carrie Purcell, stage three and four only have
+    # disability weight in the presence of anemia, which we are not
+    # currently modeling. Stage five has it's own weight
+    # TODO: The only stage referenced in the weights CSV file is stage four
+    # so I'm giving that weight to both stages four and five until I get clarity
+    stage_three = ExcessMortalityState('stage_three_ckd', disability_weight=0, modelable_entity_id=2018)
+    stage_four = ExcessMortalityState('stage_four_ckd', disability_weight=get_disability_weight(healthstate_id=391), modelable_entity_id=2019)
+    stage_five = ExcessMortalityState('stage_five_ckd', disability_weight=get_disability_weight(healthstate_id=391), modelable_entity_id=2022)
 
     stage_three_transition = IncidenceRateTransition(stage_three, 'stage_three_ckd', modelable_entity_id=2019)
     stage_four_transition = IncidenceRateTransition(stage_three, 'stage_four_ckd', modelable_entity_id=2019)
