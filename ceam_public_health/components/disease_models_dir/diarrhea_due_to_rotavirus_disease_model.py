efa459170b5ae743dd794dc00f8f04eb8c34f7b1
emumford <emumford@uw.edu>
2016-11-21 16:19:01 -0800

    cleaning up diarrhea due to rota disease model

--- ceam_public_health/components/disease_models_dir/diarrhea_due_to_rotavirus_disease_model.py
+++ ceam_public_health/components/disease_models_dir/diarrhea_due_to_rotavirus_disease_model.py
@@ -1,3 +1,8 @@
+from ceam import config
+from ceam.framework.state_machine import Transition, State, TransitionSet
+from ceam_public_health.components.disease import DiseaseModel, DiseaseState, ExcessMortalityState, IncidenceRateTransition, ProportionTransition, RemissionRateTransition, DiarrheaState
+from ceam_inputs.gbd_ms_functions import get_disability_weight
+
 def diarrhea_factory():
     
     module = DiseaseModel('diarrhea_due_to_rotavirus')
@@ -18,14 +23,15 @@ def diarrhea_factory():
                                                                    'diarrhea_to_rotavirus', 
                                                                    get_etiology_incidence('rotavirus'))
     
-    healthy.transition_set.extend([diarrhea_transition])
+    healthy.transition_set.extend([diarrhea_due_to_rotavirus_transition])
 
     # TODO: After the MVS is finished, include transitions to non-fully healthy states (e.g. malnourished and stunted health states)
+    # TODO: Figure out how remission rates can be different across diarrhea due to the different etiologies
     remission_transition = RemissionRateTransition(healthy, 'healthy', modelable_entity_id=1181)
 
-    diarrhea.transition_set.append(Transition(healthy))
+    diarrhea_due_to_rotavirus.transition_set.append(Transition(healthy))
 
-    module.states.extend([healthy, diarrhea])
+    module.states.extend([healthy, diarrhea_due_to_rotavirus])
 
     return module
 
