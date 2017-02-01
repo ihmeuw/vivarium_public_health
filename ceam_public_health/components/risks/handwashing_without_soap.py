76a7bc3b614bb51f28b9dce32c49d5b42439467d
emumford <emumford@uw.edu>
2017-02-01 11:57:32 -0800

    updating handwashing to accept all risks

--- ceam_public_health/components/risks/handwashing_without_soap.py
+++ ceam_public_health/components/risks/handwashing_without_soap.py
@@ -28,23 +28,14 @@ class Handwashing:
 
     def setup(self, builder):
 
-        # TODO: Think line below is superfluous
-        self.exposure = builder.lookup(get_exposures(risk_id=238))
-
         self.randomness = builder.randomness('handwashing_without_soap')
 
-        effect_function = categorical_exposure_effect(builder.lookup(get_exposures(risk_id=238)), 'handwashing_without_soap_susceptibility')
-        risk_effects = make_gbd_risk_effects(238, [
-            # TODO: Make this not dependent on GBD! i.e. get rid of the risk id and cause id
-            (302, 'diarrhea_due_to_rotavirus'),
-            ], 'morbidity', effect_function)
+        list_of_etiologies = ['diarrhea_due_to_shigellosis', 'diarrhea_due_to_cholera', 'diarrhea_due_to_other_salmonella', 'diarrhea_due_to_EPEC', 'diarrhea_due_to_ETEC', 'diarrhea_due_to_campylobacter', 'diarrhea_due_to_amoebiasis', 'diarrhea_due_to_cryptosporidiosis', 'diarrhea_due_to_rotaviral_entiritis', 'diarrhea_due_to_aeromonas', 'diarrhea_due_to_clostridium_difficile', 'diarrhea_due_to_norovirus', 'diarrhea_due_to_adenovirus']
 
-        return risk_effects
+        list_of_tuples = [(302, i) for i in list_of_etiologies]
 
         effect_function = categorical_exposure_effect(builder.lookup(get_exposures(risk_id=238)), 'handwashing_without_soap_susceptibility')
-        risk_effects = make_gbd_risk_effects(238, [
-            (302, 'diarrhea_due_to_rotavirus'),
-            ], 'morbidity', effect_function)
+        risk_effects = make_gbd_risk_effects(238, list_of_tuples, 'morbidity', effect_function)
 
         return risk_effects
 
