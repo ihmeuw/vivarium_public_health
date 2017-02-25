5b5b8c983d3837299865b18e2774a2fba54c1a43
Alec Deason <alecwd@uw.edu>
2017-02-24 17:04:22 -0800

    Cleaning up dependencies because I found a better way of expressing them

--- setup.py
+++ setup.py
@@ -8,15 +8,11 @@ setup(name='ceam_public_health',
         version='0.1',
         packages=find_packages(),
         include_package_data=True,
-        package_index='http://dev-tomflem.ihme.washington.edu/simple/',
         install_requires=[
             'pandas',
             'numpy',
             'scipy',
         ],
-        dependency_links=[
-            'ssh://git@stash.ihme.washington.edu:7999/cste/ceam-inputs.git#egg=ceam_inputs',
-        ]
      )
 
 
