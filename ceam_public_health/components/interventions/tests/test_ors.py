d289bc736627ad41128d5599a919bef26f470789
emumford <emumford@uw.edu>
2017-03-01 15:15:39 -0800

    deleted a bunch of files

--- ceam_public_health/components/interventions/tests/test_ors.py
+++
@@ -1,7 +0,0 @@
-def test_ors_exposure_effect():
-    # might need to adapt some of the existing tests for the categorical relative risks code
-    # test that those that receive ORS actually have a far lower mortality probability than those that have diarrhea but no ORS
-    # need to make sure that ORS count (the ors_clock section) is properly working
-
-
-# Important high-level test -- if using the GBD estimate of ORS (no artificial increase) diarrhea incidence should be the same whether or not we include ORS in the model, unless we can't calculate ORS-deleted diarrhea incidence
