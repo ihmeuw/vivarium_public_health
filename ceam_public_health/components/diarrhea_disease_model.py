1055c47a1070146b53d6d0f395948fcc2a1df440
emumford <emumford@uw.edu>
2017-03-08 09:23:10 -0800

    added severity splits to the model

--- ceam_public_health/components/diarrhea_disease_model.py
+++ ceam_public_health/components/diarrhea_disease_model.py
@@ -10,7 +10,9 @@ import numpy as np
 from ceam.framework.values import modifies_value
 from datetime import timedelta
 from ceam_public_health.components.accrue_susceptible_person_time import AccrueSusceptiblePersonTime
-
+from ceam.framework.randomness import choice
+from ceam_inputs import get_severity_splits
+from ceam_inputs import get_severe_diarrhea_excess_mortality
 
 list_of_etiologies = ['diarrhea_due_to_shigellosis', 'diarrhea_due_to_cholera', 'diarrhea_due_to_other_salmonella', 'diarrhea_due_to_EPEC', 'diarrhea_due_to_ETEC', 'diarrhea_due_to_campylobacter', 'diarrhea_due_to_amoebiasis', 'diarrhea_due_to_cryptosporidiosis', 'diarrhea_due_to_rotaviral_entiritis', 'diarrhea_due_to_aeromonas', 'diarrhea_due_to_clostridium_difficile', 'diarrhea_due_to_norovirus', 'diarrhea_due_to_adenovirus']
 
@@ -108,15 +110,20 @@ class DiarrheaBurden:
     def mortality_rates(self, index, rates, population_view):
         population = self.population_view.get(index)
 
-        return rates + self.mortality(population.index, skip_post_processor=True) * (population['diarrhea'] == 'diarrhea')
+        return rates + self.mortality(population.index, skip_post_processor=True) * (population['diarrhea'] == 'severe_diarrhea')
 
-
+    # FIXME: Need to set a priority on this function so that it is set after _move_people_into_diarrhea_state
+    # FIXME: Disability weights are severity specific. 
     @modifies_value('disability_weight')
     def disability_weight(self, index):
         population = self.population_view.get(index)
+        # self._mild_disability_weight * (population['diarrhea'] == 'mild_diarrhea')
+        # self._moderate_disability_weight * (population['diarrhea'] == 'moderate_diarrhea')
+        # self._severe_disability_weight * (population['diarrhea'] == 'severe_diarrhea')
         return self._disability_weight * (population['diarrhea'] == 'diarrhea')
 
 
+    # TODO: Confirm whether or not we need different durations for different severity levels
     # FIXME: Per conversation with Abie on 2.22, we would like to have a distribution surrounding duration
     @uses_columns(['diarrhea', 'diarrhea_event_time', 'diarrhea_event_end_time'] + list_of_etiologies)
     @listens_for('time_step', priority=8)
@@ -124,7 +131,7 @@ class DiarrheaBurden:
 
         population = event.population_view.get(event.index)
 
-        affected_population = population.query("diarrhea == 'diarrhea'").copy()
+        affected_population = population.query("diarrhea != 'healthy'").copy()
 
         affected_population['duration'] = pd.to_timedelta(self.duration(event.index), unit='D')
         affected_population['diarrhea_event_end_time'] = affected_population['duration'] + affected_population['diarrhea_event_time']
@@ -146,10 +153,6 @@ def diarrhea_factory():
     """
     list_of_modules = []
 
-    states_dict = {}
-
-    transition_dict = {}
- 
     dict_of_etiologies_and_eti_risks = {'cholera': 173, 'other_salmonella': 174, 'shigellosis': 175, 'EPEC': 176, 'ETEC': 177, 'campylobacter': 178, 'amoebiasis': 179, 'cryptosporidiosis': 180, 'rotaviral_entiritis': 181, 'aeromonas': 182, 'clostridium_difficile': 183, 'norovirus': 184, 'adenovirus': 185}
 
     for key, value in dict_of_etiologies_and_eti_risks.items():
@@ -192,6 +195,7 @@ def diarrhea_factory():
         event.population_view.update(pd.DataFrame({'diarrhea_event_time': [pd.NaT]*length}, index=event.index))
         event.population_view.update(pd.DataFrame({'diarrhea_event_end_time': [pd.NaT]*length}, index=event.index))
 
+
     # FIXME: This is a super slow function. Try to speed it up by using numbers instead of strings
     @listens_for('time_step', priority=6)
     @uses_columns(['diarrhea', 'diarrhea_event_count', 'diarrhea_event_time'] + list_of_etiologies + [i + '_event_count' for i in list_of_etiologies])
@@ -206,18 +210,26 @@ def diarrhea_factory():
         pop = pop.query("diarrhea == 'healthy'")
 
         for etiology in list_of_etiologies:
-            # etiology_pop = pop.loc[pop['{}'.format(etiology)] == etiology]
             pop.loc[pop['{}'.format(etiology)] == etiology, 'diarrhea'] = 'diarrhea'
             pop.loc[pop['{}'.format(etiology)] == etiology, '{}_event_count'.format(etiology)] += 1
 
-        pop.loc[pop['diarrhea'] == 'diarrhea', 'diarrhea_event_count'] += 1
+        pop.loc[pop['diarrhea'] == 'diarrhea', 'diarrhea_event_count'] += 1        
 
         # set diarrhea event time here
         pop.loc[pop['diarrhea'] == 'diarrhea', 'diarrhea_event_time'] = pd.Timestamp(event.time)
 
+        pop = pop.query("diarrhea == 'diarrhea'").copy()
+
+        # get diarrhea severity splits
+        mild_weight = get_severity_splits(1181, 2608)
+        moderate_weight = get_severity_splits(1181, 2609)
+        severe_weight = get_severity_splits(1181, 2610)
+
+        pop['diarrhea'] = choice('determine_diarrhea_severity', pop.index, ["mild_diarrhea", "moderate_diarrhea", "severe_diarrhea"], [mild_weight, moderate_weight, severe_weight])
+
         event.population_view.update(pop[['diarrhea', 'diarrhea_event_count', 'diarrhea_event_time'] + [i + '_event_count' for i in list_of_etiologies]])
 
-    diarrhea_burden = DiarrheaBurden(excess_mortality_data=get_excess_mortality(1181), cause_specific_mortality_data=get_cause_specific_mortality(1181), disability_weight=0.2319, duration_data=get_duration_in_days(1181))
+    diarrhea_burden = DiarrheaBurden(excess_mortality_data=get_severe_diarrhea_excess_mortality(), cause_specific_mortality_data=get_cause_specific_mortality(1181), disability_weight=0.2319, duration_data=get_duration_in_days(1181))
 
     list_of_module_and_functs = list_of_modules + [_move_people_into_diarrhea_state, _create_diarrhea_column, diarrhea_burden, AccrueSusceptiblePersonTime('diarrhea', 'diarrhea')]
 
