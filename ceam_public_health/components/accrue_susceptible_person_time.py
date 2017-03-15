fe7efe3fdff8446390398fdafd0fc4baa671d720
emumford <emumford@uw.edu>
2017-03-14 17:27:28 -0700

    cleaned up accrue_susceptible_person_time

--- ceam_public_health/components/accrue_susceptible_person_time.py
+++ ceam_public_health/components/accrue_susceptible_person_time.py
@@ -9,6 +9,29 @@ from ceam import config
 
 from ceam_inputs import get_age_bins
 
+susceptible_person_time_cols = ['susceptible_person_time_early_neonatal', 'susceptible_person_time_late_neonatal', 'susceptible_person_time_post_neonatal', 'susceptible_person_time_1_to_4', 'susceptible_person_time_5_to_9', 'susceptible_person_time_10_to_14', 'susceptible_person_time_15_to_19', 'susceptible_person_time_20_to_24', 'susceptible_person_time_25_to_29', 'susceptible_person_time_30_to_34', 'susceptible_person_time_35_to_39', 'susceptible_person_time_40_to_44', 'susceptible_person_time_45_to_49', 'susceptible_person_time_50_to_54', 'susceptible_person_time_55_to_59', 'susceptible_person_time_60_to_64', 'susceptible_person_time_65_to_69', 'susceptible_person_time_70_to_74', 'susceptible_person_time_75_to_79', 'susceptible_person_time_80_plus']
+
+# TODO: Don't duplicate code! Get rid of the duplicate lines in the block below and setup
+# get all gbd age groups
+age_bins = get_age_bins()
+
+# filter down all age groups to only the ones we care about
+# FIXME: the age groups of interest will change for GBD 2016, since the 85-90 age group is in GBD 2016, but not 2015
+age_bins = age_bins[(age_bins.age_group_id > 1) & (age_bins.age_group_id <= 21)]
+
+age_bins.age_group_name = age_bins.age_group_name.str.lower()
+
+age_bins.age_group_name = [x.strip().replace(' ', '_') for x in age_bins.age_group_name]
+
+list_of_age_bins = []
+susceptible_person_time_cols = []
+
+year_start = config.getint('simulation_parameters', 'year_start')
+
+for age_bin in pd.unique(age_bins.age_group_name.values):
+    susceptible_person_time_cols.append("susceptible_person_time_" + age_bin)
+    list_of_age_bins.append(age_bin)
+
 
 class AccrueSusceptiblePersonTime():
     # TODO: Need to figure out how pass in all of the diseases
@@ -18,29 +41,11 @@ class AccrueSusceptiblePersonTime():
 
 
     @listens_for('initialize_simulants')
-    @uses_columns(['susceptible_person_time_early_neonatal', 'susceptible_person_time_late_neonatal', 'susceptible_person_time_post_neonatal', 'susceptible_person_time_1_to_4', 'susceptible_person_time_5_to_9', 'susceptible_person_time_10_to_14', 'susceptible_person_time_15_to_19', 'susceptible_person_time_20_to_24', 'susceptible_person_time_25_to_29', 'susceptible_person_time_30_to_34', 'susceptible_person_time_35_to_39', 'susceptible_person_time_40_to_44', 'susceptible_person_time_45_to_49', 'susceptible_person_time_50_to_54', 'susceptible_person_time_55_to_59', 'susceptible_person_time_60_to_64', 'susceptible_person_time_65_to_69', 'susceptible_person_time_70_to_74', 'susceptible_person_time_75_to_79', 'susceptible_person_time_80_plus'])
+    @uses_columns(susceptible_person_time_cols)
     def create_person_year_columns(self, event):
         length = len(event.index)
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_early_neonatal': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_late_neonatal': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_post_neonatal': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_1_to_4': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_5_to_9': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_10_to_14': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_15_to_19': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_20_to_24': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_25_to_29': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_30_to_34': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_35_to_39': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_40_to_44': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_45_to_49': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_50_to_54': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_55_to_59': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_60_to_64': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_65_to_69': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_70_to_74': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_75_to_79': np.zeros(length)}, index=event.index))
-        event.population_view.update(pd.DataFrame({'susceptible_person_time_80_plus': np.zeros(length)}, index=event.index))
+        for col in susceptible_person_time_cols:
+            event.population_view.update(pd.DataFrame({col: np.zeros(length)}, index=event.index))
 
 
     def setup(self, builder):
@@ -59,7 +64,7 @@ class AccrueSusceptiblePersonTime():
 
 
     @listens_for('time_step', priority=9)
-    @uses_columns(['diarrhea', 'susceptible_person_time_early_neonatal', 'susceptible_person_time_late_neonatal', 'susceptible_person_time_post_neonatal', 'susceptible_person_time_1_to_4', 'susceptible_person_time_5_to_9', 'susceptible_person_time_10_to_14', 'susceptible_person_time_15_to_19', 'susceptible_person_time_20_to_24', 'susceptible_person_time_25_to_29', 'susceptible_person_time_30_to_34', 'susceptible_person_time_35_to_39', 'susceptible_person_time_40_to_44', 'susceptible_person_time_45_to_49', 'susceptible_person_time_50_to_54', 'susceptible_person_time_55_to_59', 'susceptible_person_time_60_to_64', 'susceptible_person_time_65_to_69', 'susceptible_person_time_70_to_74', 'susceptible_person_time_75_to_79', 'susceptible_person_time_80_plus', 'age'], 'alive')
+    @uses_columns(['diarrhea', 'age'] + susceptible_person_time_cols, 'alive')
     def count_time_steps_sim_has_diarrhea(self, event):
         pop = event.population_view.get(event.index)
 
@@ -77,31 +82,12 @@ class AccrueSusceptiblePersonTime():
 
 
     @modifies_value('metrics')
-    @uses_columns(['susceptible_person_time_early_neonatal', 'susceptible_person_time_late_neonatal', 'susceptible_person_time_post_neonatal', 'susceptible_person_time_1_to_4', 'susceptible_person_time_5_to_9', 'susceptible_person_time_10_to_14', 'susceptible_person_time_15_to_19', 'susceptible_person_time_20_to_24', 'susceptible_person_time_25_to_29', 'susceptible_person_time_30_to_34', 'susceptible_person_time_35_to_39', 'susceptible_person_time_40_to_44', 'susceptible_person_time_45_to_49', 'susceptible_person_time_50_to_54', 'susceptible_person_time_55_to_59', 'susceptible_person_time_60_to_64', 'susceptible_person_time_65_to_69', 'susceptible_person_time_70_to_74', 'susceptible_person_time_75_to_79', 'susceptible_person_time_80_plus'])
+    @uses_columns(susceptible_person_time_cols)
     def metrics(self, index, metrics, population_view):
         population = population_view.get(index)
 
-        metrics['susceptible_person_time_early_neonatal'] = population['susceptible_person_time_early_neonatal'].sum()
-        metrics['susceptible_person_time_late_neonatal'] = population['susceptible_person_time_late_neonatal'].sum()
-        metrics['susceptible_person_time_post_neonatal'] = population['susceptible_person_time_late_neonatal'].sum()
-        metrics['susceptible_person_time_1_to_4'] = population['susceptible_person_time_1_to_4'].sum()
-        metrics['susceptible_person_time_5_to_9'] = population['susceptible_person_time_5_to_9'].sum()
-        metrics['susceptible_person_time_10_to_14'] = population['susceptible_person_time_10_to_14'].sum()
-        metrics['susceptible_person_time_15_to_19'] = population['susceptible_person_time_15_to_19'].sum()
-        metrics['susceptible_person_time_20_to_24'] = population['susceptible_person_time_20_to_24'].sum()
-        metrics['susceptible_person_time_25_to_29'] = population['susceptible_person_time_25_to_29'].sum()
-        metrics['susceptible_person_time_30_to_34'] = population['susceptible_person_time_30_to_34'].sum()
-        metrics['susceptible_person_time_35_to_39'] = population['susceptible_person_time_35_to_39'].sum()
-        metrics['susceptible_person_time_40_to_44'] = population['susceptible_person_time_40_to_44'].sum()
-        metrics['susceptible_person_time_45_to_49'] = population['susceptible_person_time_45_to_49'].sum()
-        metrics['susceptible_person_time_50_to_54'] = population['susceptible_person_time_50_to_54'].sum()
-        metrics['susceptible_person_time_55_to_59'] = population['susceptible_person_time_55_to_59'].sum()
-        metrics['susceptible_person_time_60_to_64'] = population['susceptible_person_time_60_to_64'].sum()
-        metrics['susceptible_person_time_65_to_69'] = population['susceptible_person_time_65_to_69'].sum()
-        metrics['susceptible_person_time_70_to_74'] = population['susceptible_person_time_70_to_74'].sum()
-        metrics['susceptible_person_time_75_to_79'] = population['susceptible_person_time_75_to_79'].sum()
-        metrics['susceptible_person_time_80_plus'] = population['susceptible_person_time_80_plus'].sum()
-
+        for col in susceptible_person_time_cols:
+            metrics[col] = population[col].sum()
 
         return metrics
 
