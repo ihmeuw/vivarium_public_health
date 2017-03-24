bbefc9c1aaaab408c9275e72b12bdafb780d8069
emumford <emumford@uw.edu>
2017-03-23 22:47:11 -0700

    modeled underweight

--- ceam_public_health/components/risks/underweight.py
+++ ceam_public_health/components/risks/underweight.py
@@ -24,9 +24,17 @@ class Underweight:
     ------------------
     underweight_susceptibility
         Likelihood that a simulant will be underweight
+
+    Notes
+    -----
+    There are 4 categories of underweight (not underweight, mild underweight (1-2 sd below mean), moderate underweight (2-3 sd below reference mean), and severe(3+ sd below reference mean)). 1 sd below the mean is actually considered to be the TMREL 
     """
 
     def setup(self, builder):
+
+        self.exposure = builder.value('underweight.exposure')
+
+        self.exposure.source = builder.lookup(get_exposures(risk_id=94))
 
         self.randomness = builder.randomness('underweight')
 
@@ -34,7 +42,7 @@ class Underweight:
 
         list_of_tuples = [(302, i) for i in list_of_etiologies]
 
-        effect_function = categorical_exposure_effect(builder.lookup(get_exposures(risk_id=94)), 'underweight_susceptibility')
+        effect_function = categorical_exposure_effect(self.exposure, 'underweight_susceptibility')
         risk_effects = make_gbd_risk_effects(94, list_of_tuples, effect_function)
 
         return risk_effects
