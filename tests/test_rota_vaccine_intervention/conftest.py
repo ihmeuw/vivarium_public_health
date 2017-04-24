728f85addedd379036572d13c9431adb6014fa27
emumford <emumford@uw.edu>
2017-04-24 15:07:14 -0700

    updated conftest for rota vaccine tests

--- tests/test_rota_vaccine_intervention/conftest.py
+++ tests/test_rota_vaccine_intervention/conftest.py
@@ -3,5 +3,6 @@ from ceam import config
 config.set('rota_vaccine', 'age_at_first_dose', '6')
 config.set('rota_vaccine', 'age_at_second_dose', '12')
 config.set('rota_vaccine', 'age_at_third_dose', '18')
-config.set('rota_vaccine', 'time_after_dose_at_which_immunity_is_conferred', '0') 
-
+config.set('rota_vaccine', 'time_after_dose_at_which_immunity_is_conferred', '1') 
+config.set('rota_vaccine', 'vaccine_duration', '20')
+config.set('rota_vaccine', 'waning_immunity_time', '20')
