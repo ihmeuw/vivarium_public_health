0965d0d31727b91199d424e3909fc81064dc6b9a
emumford <emumford@uw.edu>
2017-05-02 15:43:26 -0700

    added time_step to conftest

--- tests/test_diarrhea_model/conftest.py
+++ tests/test_diarrhea_model/conftest.py
@@ -2,3 +2,4 @@ from ceam import config
 
 config.set('simulation_parameters', 'initial_age', '')
 config.set('simulation_parameters', 'year_start', '2005')
+config.set('simulation_parameters', 'time_step', '1')
