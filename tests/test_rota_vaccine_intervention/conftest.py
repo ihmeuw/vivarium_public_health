ab5c73ddb12966619fb7e1261a06d30aa0f9b11e
James Collins <collijk@uw.edu>
2017-05-25 12:21:55 -0700

    Made conftest use safer access to ceam config

--- tests/test_rota_vaccine_intervention/conftest.py
+++ tests/test_rota_vaccine_intervention/conftest.py
@@ -1,17 +1,25 @@
+import os
 from ceam import config
-
 # Remove user overrides but keep custom cache locations if any
-config.reset_layer('override', preserve_keys=['input_data.intermediary_data_cache_path', 'input_data.auxiliary_data_folder'])
+try:
+    config.reset_layer('override', preserve_keys=['input_data.intermediary_data_cache_path', 'input_data.auxiliary_data_folder'])
+except KeyError:
+    pass
 
-config.rota_vaccine.age_at_first_dose= 6
-config.rota_vaccine.age_at_second_dose = 12
-config.rota_vaccine.age_at_third_dose = 18
-config.rota_vaccine.time_after_dose_at_which_immunity_is_conferred = 1
-config.rota_vaccine.vaccine_full_immunity_duration = 20
-config.rota_vaccine.waning_immunity_time = 20
-config.simulation_parameters.time_step = 1
-config.simulation_parameters.year_start = 2005
-config.simulation_parameters.initial_age = 0
-config.rota_vaccine.vaccination_proportion_increase = .1
-config.rota_vaccine.third_dose_retention = 1
-config.rota_vaccine.second_dose_retention = 1
+config.simulation_parameters.set_with_metadata('year_start', 2005, layer='override', source=os.path.realpath(__file__))
+config.simulation_parameters.set_with_metadata('year_end', 2010, layer='override', source=os.path.realpath(__file__))
+config.simulation_parameters.set_with_metadata('time_step', 1, layer='override', source=os.path.realpath(__file__))
+config.simulation_parameters.set_with_metadata('initial_age', 0, layer='override', source=os.path.realpath(__file__))
+
+config.rota_vaccine.set_with_metadata('age_at_first_dose', 6, layer='override', source=os.path.realpath(__file__))
+config.rota_vaccine.set_with_metadata('age_at_second_dose', 12, layer='override', source=os.path.realpath(__file__))
+config.rota_vaccine.set_with_metadata('age_at_third_dose', 18, layer='override', source=os.path.realpath(__file__))
+config.rota_vaccine.set_with_metadata('time_after_dose_at_which_immunity_is_conferred', 1,
+                                      layer='override', source=os.path.realpath(__file__))
+config.rota_vaccine.set_with_metadata('vaccine_full_immunity_duration', 20,
+                                      layer='override', source=os.path.realpath(__file__))
+config.rota_vaccine.set_with_metadata('waning_immunity_time', 20, layer='override', source=os.path.realpath(__file__))
+config.rota_vaccine.set_with_metadata('vaccination_proportion_increase', 0.1, layer='override', source=os.path.realpath(__file__))
+config.rota_vaccine.set_with_metadata('second_dose_retention', 1, layer='override', source=os.path.realpath(__file__))
+config.rota_vaccine.set_with_metadata('third_dose_retention', 1, layer='override', source=os.path.realpath(__file__))
+
