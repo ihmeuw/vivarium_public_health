7637baceda99d54d4fe6dcefe426e14d6cc7d4c7
emumford <emumford@uw.edu>
2017-05-30 11:21:42 -0700

    using the old risk.py file for now. need to switch to the new one before merging

--- ceam_public_health/util/risk.py
+++ ceam_public_health/util/risk.py
@@ -2,6 +2,33 @@ import numpy as np
 import pandas as pd
 
 from ceam.framework.population import uses_columns
+import re
+
+def natural_key(string_):
+    """ Sorts columns with strings and numbers naturally
+    Parameters
+    ----------
+    string_: str
+        string with letters and numbers
+    """
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+
+
+def naturally_sort_df(df):
+    """
+    grabs all of the category columns in a dataframe and then naturally sorts them
+
+    Parameters
+    ----------
+
+    df : pd.DataFrame
+
+    """
+    col_list = df.columns.tolist()
+    categories =  [c for c in col_list if "cat" in c]
+    categories = sorted(categories, key=natural_key)
+
+    return df[categories], categories
 
 
 def assign_exposure_categories(df, susceptibility_column, categories):
@@ -19,8 +46,8 @@ def assign_exposure_categories(df, susceptibility_column, categories):
     """
 
     bool_list = [c + '_bool' for c in categories]
-
-    # TODO: Confirm whether or not we want < or <=
+   
+    # TODO: Confirm whether or not we want < or <= 
     for col in categories:
         df['{}_bool'.format(col)] = df['{}'.format(col)] < df[susceptibility_column]
 
@@ -32,59 +59,7 @@ def assign_exposure_categories(df, susceptibility_column, categories):
     df['exposure_category'] = 'cat' + df['exposure_category'].astype(str)
 
     return df[['exposure_category']]
-
-
-def assign_relative_risk_value(df, categories):
-    """
-    creates an 'relative_risk_value' column that assigns simulant's relative risk based on their exposure
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-
-    susceptibility_column : pd.Series
-
-    categories : list
-        list of all of the category columns in df
-
-    """
-
-    for col in categories:
-        df.loc[(df['exposure_category'] == col, 'relative_risk_value')] = df[col]
-
-    return df
-
-
-
-def assign_exposure_categories(df, susceptibility_column, categories):
-    """
-    creates an 'exposure_category' column that assigns simulant's exposure based on their susceptibility draw
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-
-    susceptibility_column : pd.Series
-
-    categories : list
-        list of all of the category columns in df 
-    """
-
-    bool_list = [c + '_bool' for c in categories]
-
-    # TODO: Confirm whether or not we want < or <=
-    for col in categories:
-        df['{}_bool'.format(col)] = df['{}'.format(col)] < df[susceptibility_column]
-
-    df['exposure_category'] = df[bool_list].sum(axis=1)
-
-    # seems weird, but we need to add 1 to exposure category. e.g. if all values for a row in bool_list are false that simulant will be in exposure cat1, not cat0
-    df['exposure_category'] = df['exposure_category'] + 1
-
-    df['exposure_category'] = 'cat' + df['exposure_category'].astype(str)
-
-    return df[['exposure_category']]
-
+    
 
 def assign_relative_risk_value(df, categories):
     """
@@ -139,21 +114,25 @@ def categorical_exposure_effect(exposure, susceptibility_column):
     """
     @uses_columns([susceptibility_column])
     def inner(rates, rr, population_view):
+    
         pop = population_view.get(rr.index)
-        exp = exposure(pop.index)
-        # Get a list of sorted category names (e.g. ['cat1', 'cat2', ..., 'cat9', 'cat10', ...])
-        categories = sorted([column for column in list(exp) if 'cat' in column],
-                            key=lambda s: int(s.split('cat')[1]))
-        sorted_exposures = exp[categories]
-        exposure_sum = np.cumsum(sorted_exposures, axis=1)
-        # Sometimes all data is 0 for the category exposures.  Set the "no exposure" category to catch this case.
-        exposure_sum[categories[-1]] = 1  # TODO: Something better than this.
-        pop_with_exp = pop.join(exposure_sum)
-        exposure_categories = assign_exposure_categories(pop_with_exp, susceptibility_column, categories)
-        df = exposure_categories.join(rr)
-        df_with_rr = assign_relative_risk_value(df, categories)
 
-        return rates * (df_with_rr.relative_risk_value.values)
+        exp = exposure(pop.index)
+
+        exp, categories = naturally_sort_df(exp)
+
+        # cumulatively sum over exposures
+        exp = np.cumsum(exp, axis=1)
+
+        exp = pop.join(exp)
+        
+        exp = assign_exposure_categories(exp, susceptibility_column, categories)
+
+        df = exp.join(rr)
+
+        df = assign_relative_risk_value(df, categories)
+
+        return rates * (df.relative_risk_value.values)
 
     return inner
 
@@ -178,8 +157,7 @@ class RiskEffect:
         self.paf_data = paf_data
         self.cause_name = cause
         self.exposure_effect = exposure_effect
-        self.risk_name = risk_name
-
+        self.risk_name = risk_name        
 
     def setup(self, builder):
         self.rr_lookup = builder.value('relative_risk_of_{r}_on_{c}'.format(r=self.risk_name, c=self.cause_name))
@@ -193,6 +171,6 @@ class RiskEffect:
 
     def incidence_rates(self, index, rates):
         rr = self.rr_lookup(index)
-
+        
         newrr = self.exposure_effect(rates, rr)
         return newrr
