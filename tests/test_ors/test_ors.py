a89537cdd1b2bb32d5af49c9e232173642af681f
James Collins <collijk@uw.edu>
2017-06-02 14:31:50 -0700

    Naming fixes

--- tests/test_ors/test_ors.py
+++ tests/test_ors/test_ors.py
@@ -32,8 +32,8 @@ def setup():
                                                    source=os.path.realpath(__file__))
     config.simulation_parameters.set_with_metadata('location_id', 179, layer='override',
                                                    source=os.path.realpath(__file__))
-    config.ORS.set_with_metadata('ors_exposure_increase_above_baseline', .5, layer='override', source=os.path.realpath(__file__))
-    config.ORS.set_with_metadata('run_intervention', True, layer='override', source=os.path.realpath(__file__))
+    config.ors.set_with_metadata('ors_exposure_increase_above_baseline', .5, layer='override', source=os.path.realpath(__file__))
+    config.ors.set_with_metadata('run_intervention', True, layer='override', source=os.path.realpath(__file__))
 
 
 def test_determine_who_gets_ors():
@@ -45,46 +45,35 @@ def test_determine_who_gets_ors():
 
     population_size = 200000
 
-    simulation = setup_simulation([generate_test_population, Ors()] + factory,
-                                  population_size=population_size)
+    simulation = setup_simulation([generate_test_population, Ors()] + factory, population_size=population_size)
 
     # make it so that all men will get diarrhea
     inc = build_table(0)
-
     inc.loc[inc.sex == 'Male', 'rate'] = 14000
-
     rota_inc = simulation.values.get_rate('incidence_rate.rotaviral_entiritis')
-
     rota_inc.source = simulation.tables.build_table(inc)
 
     pump_simulation(simulation, iterations=1)
 
     pop = simulation.population.population
-
-    num_males_with_diarrhea = len(pop.query(
-        "sex == 'Male' and diarrhea != 'healthy'"))
-
-    ors_proportion = len(pop.query(
-        "ors_working == 1 and sex == 'Male'"))/num_males_with_diarrhea
-
-    no_ors_proportion = (len(pop.query(
-        "ors_working == 0 and sex == 'Male' and diarrhea != 'healthy'")) /
-        num_males_with_diarrhea)
+    males_with_diarrhea = pop.query("sex == 'Male' and diarrhea != 'healthy'")
+    ors_proportion = len(males_with_diarrhea.query('ors_working == 1'))/len(males_with_diarrhea)
+    no_ors_proportion = len(males_with_diarrhea.query('ors_working == 0')) / len(males_with_diarrhea)
 
     ors_exposure = get_ors_exposures()
 
-    GBD_proportion_exposed = ors_exposure.query("sex == 'Male' and age <.01")\
-        .set_index(['year']).get_value(2010, 'cat2')
-    GBD_proportion_not_exposed = ors_exposure.query("sex == 'Male' and age <.01")\
-        .set_index(['year']).get_value(2010, 'cat1')
+    GBD_proportion_exposed = ors_exposure.query(
+        "sex == 'Male' and age <.01").set_index(['year']).get_value(2010, 'cat2')
+    GBD_proportion_not_exposed = ors_exposure.query(
+        "sex == 'Male' and age <.01").set_index(['year']).get_value(2010, 'cat1')
 
     assert np.allclose(ors_proportion, (GBD_proportion_exposed +
-                       config.ORS.ors_exposure_increase_above_baseline),
+                       config.ors.ors_exposure_increase_above_baseline),
                        rtol=.05), \
         "proportion of people on ors should accurately reflect the exposure from GBD"
 
     assert np.allclose(no_ors_proportion, (GBD_proportion_not_exposed -
-                       config.ORS.ors_exposure_increase_above_baseline),
+                       config.ors.ors_exposure_increase_above_baseline),
                        rtol=.05), \
         "proportion of people on ors should accurately reflect the exposure from GBD"
 
@@ -95,13 +84,9 @@ def test_ors_working_column():
     ORS and only in the correct time window
     """
     factory = diarrhea_factory()
-
     simulation = setup_simulation([generate_test_population, Ors()] + factory)
-
     pump_simulation(simulation, iterations=1)
-
     pop = simulation.population.population
-
     ors_pop = pop.query("ors_working == 1")
 
     assert (ors_pop.diarrhea != 'healthy').all(), \
@@ -114,51 +99,35 @@ def test_mortality_rates():
     diarrhea and get ORS have the severe diarrhea mortality rate * (1 - PAF) * rr
     """
     factory = diarrhea_factory()
-
     simulation = setup_simulation([generate_test_population, Ors()] + factory)
-
     # make it so that all men will get diarrhea
     inc = build_table(0)
-
     inc.loc[inc.sex == 'Male', 'rate'] = 140000
-
     rota_inc = simulation.values.get_rate('incidence_rate.rotaviral_entiritis')
-
     rota_inc.source = simulation.tables.build_table(inc)
 
     pump_simulation(simulation, iterations=1)
 
-    excess_mortality_rate = simulation.values.get_rate(
-        'excess_mortality.diarrhea')
-
+    excess_mortality_rate = simulation.values.get_rate('excess_mortality.diarrhea')
     pop = simulation.population.population.query("sex == 'Male'")
-
     ors_pop = pop.query("ors_working == 1")
-
-    diarrhea_but_no_ors_pop = pop.query(
-        "ors_working == 0 and diarrhea != 'healthy'")
+    diarrhea_but_no_ors_pop = pop.query("ors_working == 0 and diarrhea != 'healthy'")
 
     pafs = get_ors_pafs()
-    GBD_paf = pafs.query(
-        "sex == 'Male' and age <.01").set_index(['year']).get_value(2010, 'paf')
+    GBD_paf = pafs.query("sex == 'Male' and age <.01").set_index(['year']).get_value(2010, 'paf')
 
     rrs = get_ors_relative_risks()
-    GBD_rr = rrs.query(
-        "sex == 'Male' and age <.01").set_index(['year']).get_value(2010, 'cat1')
+    GBD_rr = rrs.query("sex == 'Male' and age <.01").set_index(['year']).get_value(2010, 'cat1')
 
     mr = get_severe_diarrhea_excess_mortality()
-    GBD_mr = mr.query(
-        "sex == 'Male' and age <.01").set_index(['year']).get_value(2010, 'rate')
+    GBD_mr = mr.query("sex == 'Male' and age <.01").set_index(['year']).get_value(2010, 'rate')
 
     no_ors_diarrhea_mortality_rate = GBD_mr * (1 - GBD_paf) * GBD_rr * 1/365
     ors_diarrhea_mortality_rate = GBD_mr * (1 - GBD_paf) * 1/365
 
-    simulation_no_ors_rate = excess_mortality_rate(
-        diarrhea_but_no_ors_pop.index).unique()
+    simulation_no_ors_rate = excess_mortality_rate(diarrhea_but_no_ors_pop.index).unique()
     simulation_ors_rate = excess_mortality_rate(ors_pop.index).unique()
 
-    assert np.allclose(no_ors_diarrhea_mortality_rate, simulation_no_ors_rate,
-                       rtol=.05)
+    assert np.allclose(no_ors_diarrhea_mortality_rate, simulation_no_ors_rate, rtol=.05)
 
-    assert np.allclose(ors_diarrhea_mortality_rate, simulation_ors_rate,
-                       rtol=.05)
+    assert np.allclose(ors_diarrhea_mortality_rate, simulation_ors_rate, rtol=.05)
