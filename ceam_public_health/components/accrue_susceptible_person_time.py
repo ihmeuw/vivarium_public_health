2ba26abf22352066dc5701b514e394a4d12f72fd
emumford <emumford@uw.edu>
2017-06-07 10:52:23 -0700

    initial commit for Incidence Rate component

---
+++ ceam_public_health/components/accrue_susceptible_person_time.py
@@ -0,0 +1,156 @@
+import operator
+
+import pandas as pd, numpy as np
+
+from ceam.framework.event import listens_for
+from ceam.framework.population import uses_columns
+from ceam.framework.values import modifies_value
+from ceam import config
+
+from ceam_public_health.components.util import make_age_bin_age_group_max_dict
+
+from ceam_inputs import get_age_bins
+
+from ceam_public_health.components.util import make_cols_demographically_specific, make_age_bin_age_group_max_dict
+susceptible_person_time_cols = make_cols_demographically_specific("susceptible_person_time", 2, 5)
+diarrhea_event_count_cols = make_cols_demographically_specific('diarrhea_event_count', 2, 5)
+diarrhea_event_count_cols.append('diarrhea_event_count')
+
+# TODO: Would be nice to use age_group_name instead of age_group_high and age_group_low. Using age_group_name is more specific, will make the graphs cleaner, and is more interpretable for the under 1 (neonatal) age groups.
+# FIXME: Should move the epi measures code to its own class, probably its own script
+@modifies_value('epidemiological_span_measures')
+@uses_columns(['age', 'death_day', 'cause_of_death', 'alive', 'sex'] + susceptible_person_time_cols + diarrhea_event_count_cols)
+def calculate_incidence_measure(self, index, age_groups, sexes, all_locations, duration, cube, population_view):
+    root_location = config.getint('simulation_parameters', 'location_id')
+    pop = population_view.get(index)
+
+    if all_locations:
+        locations = set(pop.location) | {-1}
+    else:
+        locations = {-1}
+
+    now = self.clock()
+    window_start = now - duration
+    current_year = window_start.year
+
+
+    # FIXME: Don't want to have age_groups[0:3] hard-coded in. Need to make a component that calculates susceptible person time for all age groups so that this can be avoided
+    for low, high in age_groups[0:4]:
+        for sex in sexes:
+            for location in locations:
+                sub_pop = pop.query('age > @low and age <= @high and sex == @sex')
+                low_str = str(np.round(low, 2))
+                high_str = str(np.round(high, 2))
+                if location >= 0:
+                    sub_pop = sub_pop.query('location == @location')
+
+                # TODO: Make this more flexible. Don't want to have diarrhea hard-coded in here. Want the susceptibility column and disease column to be variables that get passed into the class.
+                # TODO: Need to figure out best place for this
+                if not sub_pop.empty:
+                    susceptible_person_time = pop["susceptible_person_time_{l}_to_{h}_in_year_{y}_among_{s}s".format(l=low_str, h=high_str, y=current_year, s=sex)].sum()
+                    num_diarrhea_cases = pop['diarrhea_event_count_{l}_to_{h}_in_year_{y}_among_{s}s'.format(l=low_str, h=high_str, y=current_year, s=sex)].sum()
+                    if susceptible_person_time != 0:
+                        cube = cube.append(pd.DataFrame({'measure': 'incidence', 'age_low': low, 'age_high': high, 'sex': sex, 'location': location if location >= 0 else root_location, 'cause': 'diarrhea', 'value': num_diarrhea_cases/susceptible_person_time, 'sample_size': len(sub_pop)}, index=[0]).set_index(['measure', 'age_low', 'age_high', 'sex', 'location', 'cause']))
+    return cube
+
+class CalculateIncidence():
+    def __init__(self, disease_col, susceptible_col, age_group_id_min, age_group_id_max):
+        self.disease_col = disease_col
+        self.susceptible_col = susceptible_col
+        self.age_group_id_min = age_group_id_min
+        self.age_group_id_max = age_group_id_max
+
+    @listens_for('initialize_simulants')
+    @uses_columns(susceptible_person_time_cols)
+    def create_person_year_columns(self, event):
+        length = len(event.index)
+        for col in susceptible_person_time_cols:
+            event.population_view.update(pd.DataFrame({col: np.zeros(length)}, index=event.index))
+
+
+    def setup(self, builder):
+        self.sorted_dict = make_age_bin_age_group_max_dict(age_group_id_min=age_group_id_min, age_group_id_max=age_group_id_max)
+
+
+    @listens_for('time_step', priority=9)
+    @uses_columns(['diarrhea', 'age', 'sex'] + susceptible_person_time_cols, 'alive')
+    def count_time_steps_sim_has_diarrhea(self, event):
+        pop = event.population_view.get(event.index)
+
+        current_year = pd.Timestamp(event.time).year
+
+        last_age_group_max = 0
+
+        for sex in ["Male", "Female"]:
+            for key, value in self.sorted_dict:
+                # FIXME: Do we want to calculate incidence rates on a per population or susceptible person time basis?
+                # FIXME: Susceptible person time estimates are off unless end data falls exactly on a time step (so this is fine for the diarrhea model -- 1 day timesteps -- but may not be ok for other causes)
+                pop.loc[(pop[self.disease_col] != self.susceptible_col) & (pop['age'] < value) & (pop['age'] >= last_age_group_max) & (pop['sex'] == sex), 'susceptible_person_time_{k}_in_year_{c}_among_{s}s'.format(k=key, c=current_year, s=sex)] += config.simulation_parameters.time_step
+                last_age_group_max = value
+
+        event.population_view.update(pop)
+
+
+#    @modifies_value('metrics')
+#    @uses_columns(['cause_of_death', 'death_day'] + susceptible_person_time_cols)
+#    def calculate_mortality_rates(self, index, metrics, population_view):
+#        pop = population_view.get(index)
+
+#        mortality_df = pd.DataFrame(columns=['measure', 'age_low', 'age_high', 'sex', 'location', 'cause', 'value', 'year', 'draw'])
+
+#        last_age_group_max = 0
+
+#        pop['death_year'] = pop['death_day'].map(lambda x: x.year)
+
+#        for key, value in self.sorted_dict:
+#            for year in range(year_start, year_end+1):
+#                for sex in ['Male', 'Female']:
+#                    susceptible_person_time = pop["susceptible_person_time_{a}_in_year_{y}_among_{s}s".format(a=key, y=year, s=sex)].sum()
+#                    deaths_due_to_diarrhea = len(pop.query("death_year == {} and cause_of_death=='death_due_to_severe_diarrhea'".format(year)))
+#                    if susceptible_person_time != 0:
+#                        metrics["mortality_rate_" + key + "_in_year_{}".format(year) + "_among_" + sex + "s"] = deaths_due_to_diarrhea / susceptible_person_time
+#                        row = pd.DataFrame({'measure': ['mortality'], 'age_low': [last_age_group_max], 'age_high': [value], 'sex': [sex], 'location': [180], 'cause': ['diarrhea'], 'value': [deaths_due_to_diarrhea / susceptible_person_time], 'year': [year], 'draw': [0]})
+#                        mortality_df = mortality_df.append(row)
+#            last_age_group_max = value
+
+        # mortality_df.to_hdf("/share/scratch/users/emumford/mortality_rate.hdf", key="key")
+
+#        return metrics
+
+
+#    @modifies_value('metrics')
+#    @uses_columns(['cause_of_death', 'death_day'] + susceptible_person_time_cols + diarrhea_event_count_cols)
+#    def calculate_incidence_rates(self, index, metrics, population_view):
+#        pop = population_view.get(index)
+
+#        incidence_df = pd.DataFrame(columns=['measure', 'age_low', 'age_high', 'sex', 'location', 'cause', 'value', 'year', 'draw'])
+
+#        last_age_group_max = 0
+
+#        for key, value in self.sorted_dict:
+#            for year in range(year_start, year_end+1):
+#                for sex in ['Male', 'Female']:
+#                    susceptible_person_time = pop["susceptible_person_time_{a}_in_year_{y}_among_{s}s".format(a=key, y=year, s=sex)].sum()
+#                    num_diarrhea_cases = pop['diarrhea_event_count_{a}_in_year_{y}_among_{s}s'.format(a=key, y=year, s=sex)].sum()
+#                    if susceptible_person_time != 0:
+#                        metrics["incidence_rate_" + key + "_in_year_{}".format(year) + "_among_" + sex + "s"] = num_diarrhea_cases / susceptible_person_time
+#                        row = pd.DataFrame({'measure': ['incidence'], 'age_low': [last_age_group_max], 'age_high': [value], 'sex': [sex], 'location': [180], 'cause': ['diarrhea'], 'value': [num_diarrhea_cases / susceptible_person_time], 'year': [year], 'draw': [0]})
+#                        incidence_df = incidence_df.append(row)
+#            last_age_group_max = value
+
+        # incidence_df = incidence_df.to_hdf("/share/scratch/users/emumford/incidence_rate.hdf", key="key")
+
+#        return metrics
+
+    # @modifies_value('metrics')
+    # @uses_columns(susceptible_person_time_cols)
+    # def metrics(self, index, metrics, population_view):
+    #    population = population_view.get(index)
+
+    #    for col in susceptible_person_time_cols:
+    #        metrics[col] = population[col].sum()
+
+    #    return metrics
+
+
+# End.
