70e3770f2651dba4688b0d3232cc5563529c671e
emumford <emumford@uw.edu>
2017-06-15 12:38:17 -0700

    starting to write a test for calculate_incidence

---
+++ tests/metrics/test_calculate_incidence.py
@@ -0,0 +1,33 @@
+import os
+
+from ceam import config
+
+from ceam_tests.util import build_table, setup_simulation, generate_test_population, pump_simulation
+
+from ceam_public_health.metrics.calculate_incidence import CalculateIncidence
+
+def setup():
+    # Remove user overrides but keep custom cache locations if any
+    try:
+        config.reset_layer('override', preserve_keys=['input_data.intermediary_data_cache_path',
+                                                      'input_data.auxiliary_data_folder'])
+    except KeyError:
+        pass
+    config.simulation_parameters.set_with_metadata('year_start', 2005, layer='override',
+                                                   source=os.path.realpath(__file__))
+    config.simulation_parameters.set_with_metadata('year_end', 2010, layer='override',
+                                                   source=os.path.realpath(__file__))
+    config.simulation_parameters.set_with_metadata('time_step', 365, layer='override',
+                                                   source=os.path.realpath(__file__))
+    config.simulation_parameters.set_with_metadata('num_simulants', 1000, layer='override',
+                                                   source=os.path.realpath(__file__))
+    return config
+    
+def test_calculate_incidence():
+    simulation = setup_simulation([generate_test_population, CalculateIncidence])
+
+    pop = simulation.population.population
+
+    pop['diarrhea'] = ['healthy'] * 500 + ['diarrhea'] * 500
+
+    # get_value 'epidemiological_span_measures'
