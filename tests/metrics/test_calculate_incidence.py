54733077eeedccdb5445dd626e72dc5132649d3e
James Collins <collijk@uw.edu>
2017-06-20 11:06:19 -0700

    Removed pytest decorator

--- tests/metrics/test_calculate_incidence.py
+++ tests/metrics/test_calculate_incidence.py
@@ -3,11 +3,12 @@ from datetime import timedelta
 
 from ceam import config
 
-from ceam_tests.util import build_table, setup_simulation, generate_test_population, pump_simulation
+from ceam_tests.util import setup_simulation, generate_test_population, pump_simulation
 
 from ceam_public_health.metrics.calculate_incidence import CalculateIncidence
 from ceam_public_health.metrics.epidemiology import EpidemiologicalMeasures
 from ceam_public_health.experiments.diarrhea.components.diarrhea import diarrhea_factory
+
 
 def setup():
     # Remove user overrides but keep custom cache locations if any
@@ -26,15 +27,16 @@ def setup():
                                                    source=os.path.realpath(__file__))
     return config
 
-# FIXME: test_calculate_incidence isn't testing anything right now. need to figure out how to access the incidence rate value in epidemiological_span_measures
-@pytest.mark.xfail
+
+# FIXME: test_calculate_incidence isn't testing anything right now. need to
+# figure out how to access the incidence rate value in epidemiological_span_measures
 def test_calculate_incidence():
     factory = diarrhea_factory()
-
-    simulation = setup_simulation([generate_test_population, CalculateIncidence(disease_col='diarrhea', disease='diarrhea', disease_states=['mild_diarrhea']), EpidemiologicalMeasures()] + factory)
-
+    simulation = setup_simulation([generate_test_population, CalculateIncidence(disease_col='diarrhea',
+                                                                                disease='diarrhea',
+                                                                                disease_states=['mild_diarrhea']),
+                                   EpidemiologicalMeasures()] + factory)
     simulation.population.population['diarrhea'] = ['healthy'] * 50 + ['mild_diarrhea'] * 50
-
     pump_simulation(simulation, duration=timedelta(days=730))
 
     inc = simulation.values.get_value('epidemiological_span_measures')
