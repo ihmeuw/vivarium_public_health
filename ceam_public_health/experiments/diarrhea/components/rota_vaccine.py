4cae66382dd45d2b277331425f8d91d2aabbf39b
emumford <emumford@uw.edu>
2017-06-27 18:00:00 -0700

    added a FIXME

--- ceam_public_health/experiments/diarrhea/components/rota_vaccine.py
+++ ceam_public_health/experiments/diarrhea/components/rota_vaccine.py
@@ -194,6 +194,7 @@ class RotaVaccine:
         self.randomness['dose_2'] = builder.randomness('second_dose_randomness')
         self.randomness['dose_3'] = builder.randomness('third_dose_randomness')
 
+        # TODO: Determine if there is a cleaner way to incorporate DTP3 coverage
         if config.simulation_parameters.dtp3_coverage:
             self.vaccine_coverage = builder.value('{}_vaccine_coverage'.format(self.etiology))
             self.vaccine_coverage.source = builder.lookup(get_dtp3_coverage())
