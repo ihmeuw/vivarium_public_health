852ac03c7f86a3070e90ec93682bd8717b1e843b
James Collins <collijk@uw.edu>
2017-07-02 19:41:23 -0700

    Separate disability from metrics

--- ceam_public_health/metrics/__init__.py
+++ ceam_public_health/metrics/__init__.py
@@ -2,3 +2,5 @@ from .epidemiology import EpidemiologicalMeasures
 from .inspector import inspect
 from .metrics import Metrics
 from .sample_history import SampleHistory
+from .disability import Disability
+from .calculate_incidence import CalculateIncidence
