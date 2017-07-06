had order of operations wrong for paf calculation before. fixed now

ceam_public_health/experiments/diarrhea/components/rota_vaccine.py
@@ -490,7 +490,7 @@ class RotaVaccine:
         # if the vaccine has been introduced to the country, start getting the risk-deleted incidence
         # use the true coverage here. only want to risk-delete where it's necessary
         if not np.all(self.vaccine_coverage(index) == 0):
-            pafs = (1 - self.true_coverage(index) * (self.vaccine_rr - 1)) / (1 - self.true_coverage(index) * (self.vaccine_rr - 1) + 1)
+            pafs = ((1 - self.true_coverage(index)) * (self.vaccine_rr - 1)) / ((1 - self.true_coverage(index)) * (self.vaccine_rr - 1) + 1)
             rates *= pafs
     
             dose_not_working_index = population.query("rotaviral_entiritis_vaccine_third_dose_is_working == 0").index

tests/experiments/diarrhea/test_rota_vaccine.py
@@ -17,6 +17,8 @@ from ceam_public_health.experiments.diarrhea.components.rota_vaccine import (set
                                                                              determine_vaccine_protection,
                                                                              wane_immunity)
 
+from ceam_inputs import get_rota_vaccine_rrs
+
 
 def setup():
     try:
@@ -25,9 +27,9 @@ def setup():
     except KeyError:
         pass
 
-    config.simulation_parameters.set_with_metadata('year_start', 2005, layer='override',
+    config.simulation_parameters.set_with_metadata('year_start', 2010, layer='override',
                                                    source=os.path.realpath(__file__))
-    config.simulation_parameters.set_with_metadata('year_end', 2010, layer='override',
+    config.simulation_parameters.set_with_metadata('year_end', 2015, layer='override',
                                                    source=os.path.realpath(__file__))
     config.simulation_parameters.set_with_metadata('time_step', 1, layer='override',
                                                    source=os.path.realpath(__file__))
@@ -35,7 +37,6 @@ def setup():
                                                    source=os.path.realpath(__file__))
     config.simulation_parameters.set_with_metadata('location_id', 179, layer='override',
                                                    source=os.path.realpath(__file__))
-
     config.rota_vaccine.set_with_metadata('age_at_first_dose', 6, layer='override', source=os.path.realpath(__file__))
     config.rota_vaccine.set_with_metadata('age_at_second_dose', 12, layer='override', source=os.path.realpath(__file__))
     config.rota_vaccine.set_with_metadata('age_at_third_dose', 18, layer='override', source=os.path.realpath(__file__))
@@ -63,6 +64,8 @@ def test_determine_who_should_receive_dose():
                                           source=os.path.realpath(__file__))
     config.rota_vaccine.set_with_metadata('third_dose_retention', 0.4, layer='override',
                                           source=os.path.realpath(__file__))
+    config.rota_vaccine.set_with_metadata('dtp3_coverage', 0, layer='override',
+                                          source=os.path.realpath(__file__))
 
     rv_instance = RotaVaccine()
     simulation = setup_simulation([generate_test_population, rv_instance] + diarrhea_factory(), population_size=10000)
@@ -71,7 +74,7 @@ def test_determine_who_should_receive_dose():
     pop['rotaviral_entiritis_vaccine_first_dose'] = 0
     pop['age'] = config.rota_vaccine.age_at_first_dose / 365
 
-    first_dose_pop = rv_instance.determine_who_should_receive_dose(pop, 'rotaviral_entiritis_vaccine', 1)
+    first_dose_pop = rv_instance.determine_who_should_receive_dose(pop, 'rotaviral_entiritis_vaccine', 1, simulation.current_time)
 
     # FIXME: This test will fail in years in which there is vaccination coverage in the baseline scenario
     err_msg = ("Determine who should receive dose should give doses "
@@ -81,7 +84,7 @@ def test_determine_who_should_receive_dose():
 
     first_dose_pop['rotaviral_entiritis_vaccine_second_dose'] = 0
     first_dose_pop['age'] = config.rota_vaccine.age_at_second_dose / 365
-    second_dose_pop = rv_instance.determine_who_should_receive_dose(first_dose_pop, 'rotaviral_entiritis_vaccine', 2)
+    second_dose_pop = rv_instance.determine_who_should_receive_dose(first_dose_pop, 'rotaviral_entiritis_vaccine', 2, simulation.current_time)
 
     # FIXME: This test will fail in years in which there is vaccination coverage in the baseline scenario
     assert np.isclose(len(pop) * config.rota_vaccine.vaccination_proportion_increase
@@ -89,7 +92,7 @@ def test_determine_who_should_receive_dose():
 
     second_dose_pop['rotaviral_entiritis_vaccine_third_dose'] = 0
     second_dose_pop['age'] = config.rota_vaccine.age_at_third_dose / 365
-    third_dose_pop = rv_instance.determine_who_should_receive_dose(second_dose_pop, 'rotaviral_entiritis_vaccine', 3)
+    third_dose_pop = rv_instance.determine_who_should_receive_dose(second_dose_pop, 'rotaviral_entiritis_vaccine', 3, simulation.current_time)
 
     # FIXME: This test will fail in years in which there is vaccination coverage in the baseline scenario
     assert np.allclose(len(pop) * config.rota_vaccine.vaccination_proportion_increase
@@ -119,7 +122,11 @@ def test_set_vaccine_duration():
 
 @pytest.fixture
 def get_indexes():
-    simulation = setup_simulation([generate_test_population, age_simulants, RotaVaccine()])
+    simulation = setup_simulation([generate_test_population, age_simulants, RotaVaccine()], population_size=1000)
+    
+    config.rota_vaccine.set_with_metadata('dtp3_coverage', 0, layer='override',
+                                          source=os.path.realpath(__file__))
+
     pump_simulation(simulation, iterations=8)
 
     not_vaccinated = simulation.population.population.query("rotaviral_entiritis_vaccine_first_dose_is_working == 0")
@@ -131,9 +138,10 @@ def get_indexes():
 def test_set_working_column1(get_indexes):
     vaccinated, not_vaccinated = get_indexes
 
+
     err_msg = ("Working column should ensure that the correct "
                "number of simulants are receiving the benefits of the vaccine.")
-    assert np.allclose(len(vaccinated)/100, config.rota_vaccine.vaccination_proportion_increase, rtol=.1), err_msg
+    assert np.allclose(len(vaccinated)/1000, config.rota_vaccine.vaccination_proportion_increase, rtol=.1), err_msg
 
     err_msg = ("set_working_column needs to correctly identify who has "
                "been vaccinated and whether the vaccine should be conferring any benefit.")
@@ -215,6 +223,10 @@ def test_incidence_rates():
                                           source=os.path.realpath(__file__))
     config.rota_vaccine.set_with_metadata('third_dose_protection', .3, layer='override',
                                           source=os.path.realpath(__file__))
+    config.simulation_parameters.set_with_metadata('year_start', 2014, layer='override',
+                                                   source=os.path.realpath(__file__))
+    config.rota_vaccine.set_with_metadata('vaccination_proportion_increase', .4, layer='override',
+                                          source=os.path.realpath(__file__))
 
     simulation = setup_simulation([generate_test_population, age_simulants, RotaVaccine()] + diarrhea_factory())
 
@@ -235,8 +247,12 @@ def test_incidence_rates():
     err_msg = "simulants that receive vaccine should have lower incidence of diarrhea due to rota"
     a = [pd.unique(rota_inc(vaccinated.index))]
     a.append(pd.unique(rota_inc(not_vaccinated.index)*(1-vaccine_protection)))
-    assert np.allclose(pd.unique(rota_inc(vaccinated.index)),
-                       pd.unique(rota_inc(not_vaccinated.index)*(1-vaccine_protection))), err_msg
+    rr = get_rota_vaccine_rrs()
+    cov = simulation.values.get_value('rotaviral_entiritis_vaccine_coverage')
+    cov = pd.unique(cov(vaccinated.index))
+    paf = ((1 - cov) * (rr - 1)) / ((1 - cov) * (rr - 1) + 1)
+    assert np.allclose(pd.unique(rota_inc(vaccinated.index) * paf),
+                       pd.unique(rota_inc(not_vaccinated.index) * paf * rr)), err_msg
 
     # now try with two doses
     simulation = setup_simulation([generate_test_population, age_simulants, RotaVaccine()] + diarrhea_factory())