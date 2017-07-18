da901f503c2037aadae0fba40023f51ecd2cf24d
James Collins <collijk@uw.edu>
2017-07-18 09:39:48 -0700

    Finished basic tests for the data transformations.

--- tests/population/test_data_transformations.py
+++ tests/population/test_data_transformations.py
@@ -1,7 +1,10 @@
 from itertools import product
+import math
 
 import numpy as np
 import pandas as pd
+
+from vivarium.test_util import get_randomness
 
 import ceam_public_health.population.data_transformations as dt
 
@@ -49,27 +52,66 @@ def test_rescale_binned_proportions():
 
     for sex, location_id in product(['Male', 'Female'], pop_data_scaled.location_id.unique()):
         sub_pop_scaled = pop_data_scaled[(pop_data_scaled.sex == sex) & (pop_data_scaled.location_id == location_id)]
-        assert np.allclose(sub_pop_scaled['P(sex, location_id, age| year)'])
+        assert np.allclose(sub_pop_scaled['P(sex, location_id, age| year)'], p_scaled)
 
 
 def test_smooth_ages():
-    pass
+    pop_data = make_uniform_pop_data()
+    simulants = pd.DataFrame({'age': [22.5]*1000 + [52.5]*1000,
+                              'sex': ['Male', 'Female']*1000,
+                              'location': [1, 2]*1000})
+    randomness = get_randomness()
+    smoothed_simulants = dt.smooth_ages(simulants, pop_data, randomness)
 
+    assert math.isclose(len(smoothed_simulants.ages.unique()), len(smoothed_simulants.index), abs_tol=1)
+    # Tolerance is 3*std_dev of the sample mean
+    assert math.isclose(smoothed_simulants.ages.mean(), 42.5, abs_tol=3*math.sqrt(13.149778198**2/2000))
+    
 
 def test__get_bins_and_proportions():
     pop_data = dt.assign_demographic_proportions(make_uniform_pop_data())
     pop_data = pop_data[(pop_data.year == 1990) & (pop_data.location_id == 1) & (pop_data.sex == 'Male')]
-    age = dt.AgeValues(current=2.5, young=0, old=5)
+    age = dt.AgeValues(current=2.5, young=0, old=7.5)
     endpoints, proportions = dt._get_bins_and_proportions(pop_data, age)
     assert endpoints.left == 0
     assert endpoints.right == 5
-    assert proportions.current ==
+    assert proportions.current == 1 / len(pop_data)
+    assert proportions.left == 1 / len(pop_data)
+    assert proportions.right == 1 / len(pop_data)
 
+    age = dt.AgeValues(current=97.5, young=92.5, old=100)
+    endpoints, proportions = dt._get_bins_and_proportions(pop_data, age)
+    assert endpoints.left == 95
+    assert endpoints.right == 100
+    assert proportions.current == 1 / len(pop_data)
+    assert proportions.left == 1 / len(pop_data)
+    assert proportions.right == 0
 
+    age = dt.AgeValues(current=22.5, young=17.5, old=27.5)
+    endpoints, proportions = dt._get_bins_and_proportions(pop_data, age)
+    assert endpoints.left == 20
+    assert endpoints.right == 25
+    assert proportions.current == 1 / len(pop_data)
+    assert proportions.left == 1 / len(pop_data)
+    assert proportions.right == 1 / len(pop_data)
 
 
 def test__construct_sampling_parameters():
+    age = dt.AgeValues(current=50, young=22, old=104)
+    endpoint = dt.EndpointValues(left=34, right=77)
+    proportion = dt.AgeValues(current=0.1, young=0.5, old=0.3)
 
+    pdf, slope, area, cdf_inflection_point = dt._construct_sampling_parameters(age, endpoint, proportion)
+
+    assert pdf.left == ((proportion.current - proportion.young)/(age.current - age.young)
+                        * (endpoint.left - age.young) + proportion.young)
+    assert pdf.right == ((proportion.old - proportion.current) / (age.old - age.current)
+                         * (endpoint.right - age.current) + proportion.current)
+    assert area == 0.5 * ((proportion.current + pdf.left)*(age.current - endpoint.left)
+                          + (pdf.right + proportion.current)*(endpoint.right - age.current))
+    assert slope.left == (proportion.current - pdf.left) / (age.current - endpoint.left)
+    assert slope.right == (pdf.right - proportion.current) / (endpoint.right - age.current)
+    assert cdf_inflection_point == 1 / (2 * area) * (proportion.age + pdf.left) * (age.current - endpoint.left)
 
 
 def test__compute_ages():
