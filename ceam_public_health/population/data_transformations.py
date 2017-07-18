417aa7dc9d563b11acbda7377e82bfce3b65fd20
James Collins <collijk@uw.edu>
2017-07-17 17:42:51 -0700

    Test cases and documentation

--- ceam_public_health/population/data_transformations.py
+++ ceam_public_health/population/data_transformations.py
@@ -1,4 +1,5 @@
 from itertools import product
+from collections import namedtuple
 
 import numpy as np
 
@@ -9,10 +10,13 @@ def assign_demographic_proportions(population_data):
     Parameters
     ----------
     population_data : pandas.DataFrame
+        Table with columns 'age', 'sex', 'year', 'location_id', and 'pop_scaled'
 
     Returns
     -------
     pandas.DataFrame
+        Table with columns 'age', 'sex', 'year', 'location_id', 'pop_scaled',
+        'P(sex, location_id, age| year)', 'P(sex, location_id | age, year)'
     """
     def normalize(sub_pop):
         return sub_pop.pop_scaled / sub_pop[sub_pop.sex == 'Both'].pop_scaled.sum()
@@ -37,8 +41,6 @@ def rescale_binned_proportions(pop_data, pop_age_start, pop_age_end):
     -------
     pandas.DataFrame
     """
-    if pop_age_start is None or pop_age_end is None:
-        raise ValueError("Must provide initial_age if pop_age_start and/or pop_age_end are not set.")
 
     if pop_age_start != pop_data.age_group_start.min():
         pop_data = pop_data[pop_data.age_group_end > pop_age_start]
@@ -59,6 +61,10 @@ def rescale_binned_proportions(pop_data, pop_age_start, pop_age_end):
         pop_data[pop_data.sex == sex].loc[min_bin.index, 'P(sex, location_id, age| year)'] *= min_scale
 
     return pop_data
+
+
+AgeValues = namedtuple('AgeValues', ['current', 'young', 'old'])
+EndpointValues = namedtuple('EndpointValues', ['left', 'right'])
 
 
 def smooth_ages(simulants, population_data, randomness):
@@ -82,52 +88,108 @@ def smooth_ages(simulants, population_data, randomness):
         older = ages[1:] + [float(pop_data.loc[pop_data.age == ages[-1], 'age_group_end'])]
         uniform_all = randomness.get_draw(simulants.index, additional_key='smooth_ages')
 
-        for age, young, old in zip(ages, younger, older):
-            affected = simulants[(simulants.age == age) & (simulants.sex == sex) & (simulants.location == location_id)]
+        for age_set in zip(ages, younger, older):
+            age = AgeValues(*age_set)
+            affected = simulants[(simulants.age == age.current)
+                                 & (simulants.sex == sex)
+                                 & (simulants.location == location_id)]
             # bin endpoints
-            left = float(pop_data.loc[pop_data.age == age, 'age_group_start'])
-            right = float(pop_data.loc[pop_data.age == age, 'age_group_end'])
-
-            # proportion in this bin and the neighboring bins
-            proportion_column = 'P(sex, location_id, age| year)'
-            p_age = float(pop_data.loc[pop_data.age == age, proportion_column])
-            p_young = float(pop_data.loc[pop_data.age == young, proportion_column]) if young != left else p_age
-            p_old = float(pop_data.loc[pop_data.age == old, proportion_column]) if old != right else 0
-
-            # pdf value at bin endpoints
-            f_left = (p_age - p_young)/(age - young)*(left - young) + p_young
-            f_right = (p_old - p_age)/(old - age)*(right - age) + p_age
-
-            # normalization constant.  Total area under pdf.
-            area = 0.5*((p_age + f_left)*(age - left) + (f_right + p_age)*(right - age))
-
-            # pdf slopes.
-            m_left = (p_age - f_left)/(age - left)
-            m_right = (f_right - p_age)/(right - age)
-
-            # The decision bound on the uniform rv.
-            cdf_at_age = 1/(2*area)*(p_age + f_left)*(age - left)
+            endpoints, proportions = _get_bins_and_proportions(pop_data, age)
+            pdf, slope, area, cdf_inflection_point = _construct_sampling_parameters(age, endpoints, proportions)
 
             # Make a draw from a uniform distribution
             uniform_rv = uniform_all.iloc[affected.index]
 
-            left_sims = affected[uniform_rv <= cdf_at_age]
-            right_sims = affected[uniform_rv > cdf_at_age]
+            left_sims = affected[uniform_rv <= cdf_inflection_point]
+            right_sims = affected[uniform_rv > cdf_inflection_point]
 
-            # Compute and assign ages.
-            if m_left == 0:
-                simulants.loc[left_sims.index, 'age'] = left + area/f_left*uniform_rv[left_sims.index]
-            else:
-                simulants.loc[left_sims.index, 'age'] = left + f_left/m_left*(
-                    np.sqrt(1 + 2*area*m_left/f_left**2*uniform_rv[left_sims.index]) - 1)
-
-            if m_right == 0:
-                simulants.loc[right_sims.index, 'age'] = age + area/p_age*(uniform_rv[right_sims.index] - cdf_at_age)
-            else:
-                simulants.loc[right_sims.index, 'age'] = age + p_age/m_right*(
-                    np.sqrt(1 + 2*area*m_right/p_age**2*(uniform_rv[right_sims.index] - cdf_at_age)) - 1)
+            simulants.loc[left_sims.index, 'age'] = _compute_ages(uniform_rv[left_sims.index],
+                                                                  endpoints.left, pdf.left, slope.left, area)
+            simulants.loc[right_sims.index, 'age'] = _compute_ages(uniform_rv[right_sims.index],
+                                                                   endpoints.right, pdf.right, slope.right, area)
 
     return simulants
+
+
+def _get_bins_and_proportions(pop_data, age):
+    """
+    Parameters
+    ----------
+    pop_data : pandas.DataFrame
+    age : AgeValues
+
+    Returns
+    -------
+    (EndpointValues, AgeValues)
+    """
+    left = float(pop_data.loc[pop_data.age == age.current, 'age_group_start'])
+    right = float(pop_data.loc[pop_data.age == age.current, 'age_group_end'])
+
+    # proportion in this bin and the neighboring bins
+    proportion_column = 'P(sex, location_id, age| year)'
+    p_age = float(pop_data.loc[pop_data.age == age.current, proportion_column])
+    p_young = float(pop_data.loc[pop_data.age == age.young, proportion_column]) if age.young != left else p_age
+    p_old = float(pop_data.loc[pop_data.age == age.old, proportion_column]) if age.old != right else 0
+
+    return EndpointValues(left, right), AgeValues(p_age, p_young, p_old)
+
+
+def _construct_sampling_parameters(age, endpoint, proportion):
+    """
+    Parameters
+    ----------
+    age : AgeValues
+    endpoint : EndpointValues
+    proportion : AgeValues
+
+    Returns
+    -------
+    (pdf, slope, area, cdf_inflection_point) : (EndpointValues, EndpointValues, float, float)
+    """
+    # pdf value at bin endpoints
+    pdf_left = ((proportion.current - proportion.young)/(age.current - age.young)
+                * (endpoint.left - age.young) + proportion.young)
+    pdf_right = ((proportion.old - proportion.current) / (age.old - age.current)
+                 * (endpoint.right - age.current) + proportion.current)
+    pdf = EndpointValues(pdf_left, pdf_right)
+
+    # normalization constant.  Total area under pdf.
+    area = 0.5 * ((proportion.current + pdf.left)*(age.current - endpoint.left)
+                  + (pdf.right + proportion.current)*(endpoint.right - age.current))
+
+    # pdf slopes.
+    m_left = (proportion.current - pdf.left) / (age.current - endpoint.left)
+    m_right = (pdf.right - proportion.current) / (endpoint.right - age.current)
+    slope = EndpointValues(m_left, m_right)
+
+    # The decision bound on the uniform rv.
+    cdf_inflection_point = 1 / (2 * area) * (proportion.age + pdf.left) * (age.current - endpoint.left)
+
+    return pdf, slope, area, cdf_inflection_point
+
+
+def _compute_ages(uniform_rv, start, height, slope, normalization):
+    """
+    Parameters
+    ----------
+    uniform_rv : numpy.ndarray or float
+    start : float
+    height : float
+    slope : float
+    normalization : float
+
+    Returns
+    -------
+    numpy.ndarray or float
+    """
+    if slope == 0:
+        return start + normalization/height*uniform_rv
+    else:
+        return start + height/slope*(np.sqrt(1 + 2*normalization*slope / height**2 * uniform_rv) - 1)
+
+
+
+
 
 
 
--- tests/population/test_data_transformations.py
+++ tests/population/test_data_transformations.py
@@ -1,9 +1,77 @@
+from itertools import product
+
+import numpy as np
+import pandas as pd
+
+import ceam_public_health.population.data_transformations as dt
+
+
+def make_uniform_pop_data():
+    age_bins = [(n, n+2.5, n+5) for n in range(0, 100, 5)]
+    sexes = ('Male', 'Female', 'Both')
+    years = (1990, 1995, 2000, 2005)
+    locations = (1, 2)
+
+    age_bins, sexes, years, locations = zip(*product(age_bins, sexes, years, locations))
+    mins, ages, maxes = zip(*age_bins)
+    pop = pd.DataFrame({'age': ages,
+                        'age_group_start': mins,
+                        'age_group_end': maxes,
+                        'sex': sexes,
+                        'year': years,
+                        'location_id': locations,
+                        'pop_scaled': [100]*len(ages)})
+    pop.loc[pop.sex == 'Both', 'pop_scaled'] = 200
+    return pop
+
 
 def test_assign_demographic_proportions():
-    pass
+    pop_data = dt.assign_demographic_proportions(make_uniform_pop_data())
+
+    assert not pop_data[pop_data.sex == 'Both'].any()
+    # The make_uniform_pop_data has four years by default, so we should have four uniform conditional pdfs
+    assert np.allclose(pop_data['P(sex, location_id, age| year)'], 4 / len(pop_data))
+    # There is a single location and the sexes are uniformly distributed.
+    assert np.allclose(pop_data['P(sex, location_id | age, year)'], 1/2)
+
 
 def test_rescale_binned_proportions():
-    pass
+    pop_data = dt.assign_demographic_proportions(make_uniform_pop_data())
+    pop_data = pop_data[pop_data.year == 1990]
+
+    pop_data_scaled = dt.rescale_binned_proportions(pop_data, pop_age_start=0, pop_age_end=100)
+    # Should be a no-op
+    assert np.allclose(pop_data['P(sex, location_id, age| year)'], pop_data_scaled['P(sex, location_id, age| year)'])
+
+    pop_data_scaled = dt.rescale_binned_proportions(pop_data, pop_age_start=2.5, pop_age_end=7.5)
+    base_p = 1/len(pop_data)
+    p_scaled = [base_p/2, base_p, base_p/2]
+
+    for sex, location_id in product(['Male', 'Female'], pop_data_scaled.location_id.unique()):
+        sub_pop_scaled = pop_data_scaled[(pop_data_scaled.sex == sex) & (pop_data_scaled.location_id == location_id)]
+        assert np.allclose(sub_pop_scaled['P(sex, location_id, age| year)'])
+
 
 def test_smooth_ages():
     pass
+
+
+def test__get_bins_and_proportions():
+    pop_data = dt.assign_demographic_proportions(make_uniform_pop_data())
+    pop_data = pop_data[(pop_data.year == 1990) & (pop_data.location_id == 1) & (pop_data.sex == 'Male')]
+    age = dt.AgeValues(current=2.5, young=0, old=5)
+    endpoints, proportions = dt._get_bins_and_proportions(pop_data, age)
+    assert endpoints.left == 0
+    assert endpoints.right == 5
+    assert proportions.current ==
+
+
+
+
+def test__construct_sampling_parameters():
+
+
+
+def test__compute_ages():
+    assert dt._compute_ages(1, 10, 12, 0, 33) == 10 + 33/12*1
+    assert dt._compute_ages(1, 10, 12, 5, 33) == 10 + 12/5*(np.sqrt(1+2*33*5/12**2*1) - 1)
