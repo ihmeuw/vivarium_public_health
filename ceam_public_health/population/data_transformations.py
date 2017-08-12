59b83d875cc51aa0179c50abbbfd845193c8f29c
Alec Deason <alecwd@uw.edu>
2017-08-11 17:03:00 -0700

    ignore rows where como gives us data but codcorrect doesn't

--- ceam_public_health/population/data_transformations.py
+++ ceam_public_health/population/data_transformations.py
@@ -271,5 +271,5 @@ def get_cause_deleted_mortality(all_cause_mortality, list_of_csmrs):
     for csmr in list_of_csmrs:
         if csmr is None:
             continue
-        all_cause_mortality = all_cause_mortality.subtract(csmr.set_index(index_cols))
+        all_cause_mortality = all_cause_mortality.subtract(csmr.set_index(index_cols)).dropna()
     return all_cause_mortality.reset_index().rename(columns={'rate': 'death_due_to_other_causes'})
