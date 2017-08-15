853d2e8bce567b9d48c2aa1bb5a798ddfe0e6688
James Collins <collijk@uw.edu>
2017-08-15 15:10:39 -0700

    Added duration

--- ceam_public_health/disease/factories.py
+++ ceam_public_health/disease/factories.py
@@ -1,4 +1,8 @@
-from ceam_inputs import get_disability_weight, get_prevalence, get_excess_mortality
+import numbers
+
+import pandas as pd
+
+from ceam_inputs import get_disability_weight, get_prevalence, get_excess_mortality, get_duration
 
 from .state import DiseaseState, ExcessMortalityState
 
@@ -7,15 +11,18 @@ def make_disease_state(cause, side_effect_function=None):
     prevalence = get_prevalence(cause)
     disability_weight = get_disability_weight(cause)
     excess_mortality = get_excess_mortality(cause)
+    dwell_time = get_duration(cause)
 
-    if excess_mortality:
-        return ExcessMortalityState(cause,
+    if isinstance(excess_mortality, pd.DataFrame) or excess_mortality > 0:
+        return ExcessMortalityState(cause.name,
                                     prevalence_data=prevalence,
+                                    dwell_time=dwell_time,
                                     disability_weight=disability_weight,
                                     excess_mortality_data=excess_mortality,
                                     side_effect_function=side_effect_function)
-    return DiseaseState(cause,
+    return DiseaseState(cause.name,
                         prevalence_data=prevalence,
+                        dwell_time=dwell_time,
                         disability_weight=disability_weight,
                         side_effect_function=side_effect_function)
 
