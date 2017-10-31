4206c33230dee91ecbe627dfe7fa3e2a4c47c732
A. Flaxman <abie@alum.mit.edu>
2017-10-30 20:33:10 -0700

    TST: get simple_extrapolation test to pass

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,12 @@
 import pytest
 import numpy as np
+import pandas as pd
 
+import ceam_public_health.util
 
 def test_simple_extrapolation():
     s = pd.Series({0:0, 1:1})
-    f = ceam_experiments.cvd.components.opportunistic_screening.simple_extrapolation(s)
+    f = ceam_public_health.util.simple_extrapolation(s)
 
     assert f(0) == 0, 'should be precise at index values'
     assert f(1) == 1
