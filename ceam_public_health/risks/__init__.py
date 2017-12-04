976f6a7e89482ce72abde6173b78b4598b92e13b
James Collins <collijk@uw.edu>
2017-12-03 17:33:56 -0800

    Add get_distribution to risk/__init__

--- ceam_public_health/risks/__init__.py
+++ ceam_public_health/risks/__init__.py
@@ -1,3 +1,4 @@
+from .distributions import get_distribution
 from .exposures import get_exposure_function
 from .effect import RiskEffect, make_gbd_risk_effects
 from .base_risk import ContinuousRiskComponent, CategoricalRiskComponent
