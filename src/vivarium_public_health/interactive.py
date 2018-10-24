16160d6d16a97d279a3ad9ba2e1740d624570ca4
Kate Wilson <kwilson7@uw.edu>
2018-10-24 10:59:30 -0700

    adding type hints

--- src/vivarium_public_health/interactive.py
+++ src/vivarium_public_health/interactive.py
@@ -1,7 +1,9 @@
-from vivarium.interface import interactive
-from vivarium.framework.configuration import build_simulation_configuration, build_model_specification
+import vivarium.interface.interactive as vi
+from vivarium.framework.configuration import build_model_specification
 from vivarium.framework.plugins import PluginManager
 from vivarium.config_tree import ConfigTree
+
+from typing import Mapping, Union, List
 
 ARTIFACT_PLUGIN = {
     'optional': {
@@ -13,20 +15,21 @@ ARTIFACT_PLUGIN = {
 }
 
 
-
-def initialize_simulation(components, input_config=None, plugin_config=None):
+def initialize_simulation(components: List, input_config: Union[Mapping, ConfigTree]=None,
+                          plugin_config: Mapping=None) -> vi.InteractiveContext:
     plugin_config = ARTIFACT_PLUGIN.update(plugin_config) if plugin_config else ARTIFACT_PLUGIN
-    return interactive.initialize_simulation(components, input_config, plugin_config)
+    return vi.initialize_simulation(components, input_config, plugin_config)
 
 
-def setup_simulation(components, input_config=None, plugin_config=None):
+def setup_simulation(components: List, input_config: Union[Mapping, ConfigTree]=None,
+                     plugin_config: Mapping=None) -> vi.InteractiveContext:
     simulation = initialize_simulation(components, input_config, plugin_config)
     simulation.setup()
 
     return simulation
 
 
-def initialize_simulation_from_model_specification(model_specification_file):
+def initialize_simulation_from_model_specification(model_specification_file: str) -> vi.InteractiveContext:
     model_specification = build_model_specification(model_specification_file)
 
     plugin_config = ConfigTree(ARTIFACT_PLUGIN)
@@ -38,10 +41,10 @@ def initialize_simulation_from_model_specification(model_specification_file):
     component_config_parser = plugin_manager.get_plugin('component_configuration_parser')
     components = component_config_parser.get_components(component_config)
 
-    return interactive.InteractiveContext(simulation_config, components, plugin_manager)
+    return vi.InteractiveContext(simulation_config, components, plugin_manager)
 
 
-def setup_simulation_from_model_specification(model_specification_file):
+def setup_simulation_from_model_specification(model_specification_file: str) -> vi.InteractiveContext:
     simulation = initialize_simulation_from_model_specification(model_specification_file)
     simulation.setup()
 
