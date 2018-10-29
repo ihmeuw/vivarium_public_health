795fdff9c0802d0cb245f7d04ce34a7912475732
James Collins <collijk@uw.edu>
2018-10-29 11:39:21 -0700

    Disambiguate disease total counts, deaths due to a disease, and ending prevalent_cases

--- src/vivarium_public_health/disease/model.py
+++ src/vivarium_public_health/disease/model.py
@@ -147,5 +147,5 @@ class DiseaseModel(Machine):
 
     def metrics(self, index, metrics):
         population = self.population_view.get(index, query="alive == 'alive'")
-        metrics[self.condition + '_count'] = (population[self.condition] != 'susceptible_to_' + self.condition).sum()
+        metrics[self.condition + '_current_count'] = (population[self.condition] != 'susceptible_to_' + self.condition).sum()
         return metrics
