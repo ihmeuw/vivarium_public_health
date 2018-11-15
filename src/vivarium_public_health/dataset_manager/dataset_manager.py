1103232bdd2d0a94d70d415c8bdcfc73784efeeb
Kate Wilson <kwilson7@uw.edu>
2018-11-15 10:48:13 -0800

    adding config filter term to dataset manager load

--- src/vivarium_public_health/dataset_manager/dataset_manager.py
+++ src/vivarium_public_health/dataset_manager/dataset_manager.py
@@ -60,16 +60,21 @@ class ArtifactManager:
     configuration_defaults = {
         'input_data': {
             'artifact_path': None,
-            'artifact_filter_terms': None
+            'artifact_filter_term': None
         }
     }
 
     def setup(self, builder: Builder):
         """Performs this component's simulation setup."""
         self.artifact = self._load_artifact(builder.configuration)
+        # because not all columns are accessible via artifact filter terms, apply config filters separately
+        self.config_filter_term = validate_filter_term(builder.configuration.input_data.artifact_filter_term)
 
     def _load_artifact(self, configuration: ConfigTree) -> Artifact:
-        """Looks up the path to the artifact hdf file, builds a default filter, and generates the data artifact.
+        """Looks up the path to the artifact hdf file, builds a default filter,
+        and generates the data artifact. Stores any configuration specified filter
+        terms separately to be applied on loading, because not all columns are
+        available via artifact filter terms.
 
         Parameters
         ----------
@@ -83,7 +88,6 @@ class ArtifactManager:
         artifact_path = parse_artifact_path_config(configuration)
         draw = configuration.input_data.input_draw_number
         location = configuration.input_data.location
-        config_filter_terms = configuration.input_data.artifact_filter_terms
         base_filter_terms = [f'draw == {draw}', get_location_term(location)]
         return Artifact(artifact_path, base_filter_terms)
 
@@ -107,27 +111,31 @@ class ArtifactManager:
             The data associated with the given key, filtered down to the requested subset
             if the data is a dataframe.
         """
-        data = _config_filter(self.artifact.load(entity_key), self.config_filter_terms)
-        return filter_data(data, keep_age_group_edges, **column_filters) if isinstance(data, pd.DataFrame) else data
+        data = self.artifact.load(entity_key)
+        return filter_data(data, keep_age_group_edges, self.config_filter_term, **column_filters) if isinstance(data, pd.DataFrame) else data
 
 
-def filter_data(data: pd.DataFrame, keep_age_group_edges: bool, **column_filters: _Filter) -> pd.DataFrame:
+def filter_data(data: pd.DataFrame, keep_age_group_edges: bool,
+                config_filter_term: str=None, **column_filters: _Filter) -> pd.DataFrame:
     """Uses the provided column filters and age_group conditions to subset the raw data."""
+    data = _config_filter(data, config_filter_term)
     data = _subset_rows(data, **column_filters)
     data = _subset_columns(data, keep_age_group_edges, **column_filters)
     return data
 
-def _config_filter(data: pd.DataFrame, config_filter_terms: str) -> pd.DataFrame:
-    if config_filter_terms is not None:
-        return None
+
+def _config_filter(data, config_filter_term):
+    filter_column = re.split('[<=>]', config_filter_term.split()[0])[0]
+    if filter_column in data.columns:
+        data = data.query(config_filter_term)
+    return data
 
 
-def _parse_config_filter(data_columns, config_filter_terms):
-    ind_terms = re.split('[()]', config_filter_terms)
-
-        #re.split('[&|]').split(' and ').split(' or ')  # now we have a list of each term
-    filter_columns = [re.split()]
-
+def validate_filter_term(config_filter_term):
+    multiple_filter_indicators = [' and ', ' or ', '|', '&']
+    if config_filter_term is not None and any(x in config_filter_term for x in multiple_filter_indicators):
+        raise NotImplementedError("Only a single filter term via the configuration is currently supported.")
+    return config_filter_term
 
 
 def _subset_rows(data: pd.DataFrame, **column_filters: _Filter) -> pd.DataFrame:
