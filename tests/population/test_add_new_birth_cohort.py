6411f9cfa86c70e44d8eeedd76c23e3d0c83b2cb
Kate Wilson <kwilson7@uw.edu>
2018-11-26 13:33:28 -0800

    updating test to match column names of actual asfr data

--- tests/population/test_add_new_birth_cohort.py
+++ tests/population/test_add_new_birth_cohort.py
@@ -127,8 +127,9 @@ def test_fertility_module(base_config, base_plugins):
     components = [TestPopulation(), FertilityAgeSpecificRates()]
     simulation = initialize_simulation(components, base_config, base_plugins)
 
-    simulation.data.write("covariate.age_specific_fertility_rate.estimate",
-                          build_table(0.05, 1990, 2018).query("sex == 'Female'").drop("sex", "columns"))
+    asfr_data = build_table(0.05, 1990, 2018).query("sex == 'Female'")\
+        .drop("sex", "columns").rename(columns={'value': 'mean_value'})
+    simulation.data.write("covariate.age_specific_fertility_rate.estimate", asfr_data)
 
     simulation.setup()
 
