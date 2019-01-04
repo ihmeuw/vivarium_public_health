39e11b4f1ea4d3810d63c2c8527cabec7711a9e2
James Collins <collijk@uw.edu>
2019-01-03 17:57:19 -0800

    install script updates

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -17,7 +17,7 @@ python -V
 
 pip install --upgrade pip setuptools
 
-if [$TRAVIS_PULL_REQUEST = "false"]
+if [ $TRAVIS_PULL_REQUEST = "false" ]
 then
     branch=$TRAVIS_BRANCH
 else
@@ -30,7 +30,7 @@ upstream_branch=git ls-remote --heads git@github.com:ihmeuw/vivarium.git $branch
 
 echo upstream branch found $upstream_branch
 
-if [$upstream_branch]
+if [ $upstream_branch -eq 1]
 then
     "git clone git@github.com:ihmeuw/vivarium.git"
     pushd vivarium
