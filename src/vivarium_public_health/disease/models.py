9fc2beec6f1272b1eb73bafd9883990a3d9a14a1
James Collins <collijk@uw.edu>
2019-01-14 11:19:27 -0800

    Clean up model setup methods

--- src/vivarium_public_health/disease/models.py
+++ src/vivarium_public_health/disease/models.py
@@ -11,22 +11,21 @@ class SI:
 
     def setup(self, builder):
         only_morbid = builder.data.load(f'cause.{self.cause}.restrictions')['yld_only']
+        disease_model_data_functions = {}
 
         healthy = SusceptibleState(self.cause)
-        healthy.allow_self_transitions()
-
-        get_data_functions = {}
         if only_morbid:
             infected = DiseaseState(self.cause)
-            get_data_functions['csmr'] = lambda _, __: None
+            disease_model_data_functions['csmr'] = lambda _, __: None
         else:
             infected = ExcessMortalityState(self.cause)
+
+        healthy.allow_self_transitions()
+        healthy.add_transition(infected, source_data_type='rate')
         infected.allow_self_transitions()
 
-        healthy.add_transition(infected, source_data_type='rate')
-
         builder.components.add_components([DiseaseModel(self.cause, states=[healthy, infected],
-                                                        get_data_functions=get_data_functions)])
+                                                        get_data_functions=disease_model_data_functions)])
 
 
 class SIR:
@@ -36,27 +35,24 @@ class SIR:
 
     def setup(self, builder):
         only_morbid = builder.data.load(f'cause.{self.cause}.restrictions')['yld_only']
+        disease_model_data_functions = {}
 
         healthy = SusceptibleState(self.cause)
-        healthy.allow_self_transitions()
-
-        get_data_functions = {}
         if only_morbid:
             infected = DiseaseState(self.cause)
-            get_data_functions['csmr'] = lambda _, __: None
+            disease_model_data_functions['csmr'] = lambda _, __: None
         else:
             infected = ExcessMortalityState(self.cause)
-
-        infected.allow_self_transitions()
-
         recovered = RecoveredState(self.cause)
+
+        healthy.allow_self_transitions()
+        healthy.add_transition(infected, source_data_type='rate')
+        infected.allow_self_transitions()
+        infected.add_transition(recovered, source_data_type='rate')
         recovered.allow_self_transitions()
 
-        healthy.add_transition(infected, source_data_type='rate')
-        infected.add_transition(recovered, source_data_type='rate')
-
         builder.components.add_components([DiseaseModel(self.cause, states=[healthy, infected, recovered],
-                                                        get_data_functions=get_data_functions)])
+                                                        get_data_functions=disease_model_data_functions)])
 
 
 class SIS:
@@ -66,29 +62,27 @@ class SIS:
 
     def setup(self, builder):
         only_morbid = builder.data.load(f'cause.{self.cause}.restrictions')['yld_only']
+        disease_model_data_functions = {}
 
         healthy = SusceptibleState(self.cause)
-        healthy.allow_self_transitions()
-
-        get_data_functions = {}
         if only_morbid:
             infected = DiseaseState(self.cause)
-            get_data_functions['csmr'] = lambda _, __: None
+            disease_model_data_functions['csmr'] = lambda _, __: None
         else:
             infected = ExcessMortalityState(self.cause)
 
-        infected.allow_self_transitions()
-
+        healthy.allow_self_transitions()
         healthy.add_transition(infected, source_data_type='rate')
+        infected.allow_self_transitions()
         infected.add_transition(healthy, source_data_type='rate')
 
         builder.components.add_components([DiseaseModel(self.cause, states=[healthy, infected],
-                                                        get_data_functions=get_data_functions)])
+                                                        get_data_functions=disease_model_data_functions)])
 
 
 class SIS_fixed_duration:
 
-    def __init__(self, cause: str, duration):
+    def __init__(self, cause: str, duration: str):
         """
         Parameters
         ----------
@@ -103,30 +97,29 @@ class SIS_fixed_duration:
 
     def setup(self, builder):
         only_morbid = builder.data.load(f'cause.{self.cause}.restrictions')['yld_only']
+        disease_model_data_functions = {}
 
         healthy = SusceptibleState(self.cause)
-        healthy.allow_self_transitions()
-
-        get_data_functions = {}
         if only_morbid:
             infected = DiseaseState(self.cause,
                                     get_data_functions={'dwell_time': lambda _, __: self.duration})
-            get_data_functions['csmr'] = lambda _, __: None
+            disease_model_data_functions['csmr'] = lambda _, __: None
         else:
             infected = ExcessMortalityState(self.cause,
                                             get_data_functions={'dwell_time': lambda _, __: self.duration})
-        infected.allow_self_transitions()
 
+        healthy.allow_self_transitions()
         healthy.add_transition(infected, source_data_type='rate')
         infected.add_transition(healthy)
+        infected.allow_self_transitions()
 
         builder.components.add_components([DiseaseModel(self.cause, states=[healthy, infected],
-                                                        get_data_functions=get_data_functions)])
+                                                        get_data_functions=disease_model_data_functions)])
 
 
 class SIR_fixed_duration:
 
-    def __init__(self, cause: str, duration):
+    def __init__(self, cause: str, duration: str):
         """
         Parameters
         ----------
@@ -141,30 +134,26 @@ class SIR_fixed_duration:
 
     def setup(self, builder):
         only_morbid = builder.data.load(f'cause.{self.cause}.restrictions')['yld_only']
+        disease_model_data_functions = {}
 
         healthy = SusceptibleState(self.cause)
-
-        get_data_functions = {}
         if only_morbid:
             infected = DiseaseState(self.cause,
                                     get_data_functions={'dwell_time': lambda _, __: self.duration})
-            get_data_functions['csmr'] = lambda _, __: None
+            disease_model_data_functions['csmr'] = lambda _, __: None
         else:
             infected = ExcessMortalityState(self.cause,
                                             get_data_functions={'dwell_time': lambda _, __: self.duration})
-
         recovered = RecoveredState(self.cause)
 
         healthy.allow_self_transitions()
         healthy.add_transition(infected, source_data_type='rate')
-
         infected.allow_self_transitions()
         infected.add_transition(recovered)
-
         recovered.allow_self_transitions()
 
         builder.components.add_components([DiseaseModel(self.cause, states=[healthy, infected, recovered],
-                                                        get_data_functions=get_data_functions)])
+                                                        get_data_functions=disease_model_data_functions)])
 
 
 class neonatal:
@@ -173,18 +162,17 @@ class neonatal:
         self.cause = cause
 
     def setup(self, builder):
-
         only_morbid = builder.data.load(f'cause.{self.cause}.restrictions')['yld_only']
+        disease_model_data_functions = {}
 
         healthy = SusceptibleState(self.cause)
-        healthy.allow_self_transitions()
-
-        get_data_functions = {}
         if only_morbid:
             with_condition = DiseaseState(self.cause)
-            get_data_functions['csmr'] = lambda _, __: None
+            disease_model_data_functions['csmr'] = lambda _, __: None
         else:
             with_condition = ExcessMortalityState(self.cause)
+
+        healthy.allow_self_transitions()
         with_condition.allow_self_transitions()
 
         # TODO: some neonatal causes (e.g. sepsis) have incidence and remission at least at the MEID level
@@ -192,4 +180,4 @@ class neonatal:
         # with_condition.add_transition(healthy, source_data_type='rate')
 
         builder.components.add_components([DiseaseModel(self.cause, states=[healthy, with_condition],
-                                                        get_data_functions=get_data_functions)])
+                                                        get_data_functions=disease_model_data_functions)])
