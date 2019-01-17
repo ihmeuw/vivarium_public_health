b3e3df329cc7a04ba85c770dd4eee942c7612d0c
James Collins <collijk@uw.edu>
2019-01-16 20:55:04 -0800

    Update risk observer

--- src/vivarium_public_health/risks/observer.py
+++ src/vivarium_public_health/risks/observer.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from vivarium_public_health.risks.data_transformation import split_risk_from_type
+from vivarium_public_health.risks.data_transformations import RiskString
 
 
 class CategoricalRiskObserver:
@@ -20,8 +20,7 @@ class CategoricalRiskObserver:
     """
     configuration_defaults = {
         'risk_observer': {
-            'by_year': True,
-            'sample_date':{
+            'sample_date': {
                 'month': 7,
                 'day': 1
             }
@@ -36,53 +35,58 @@ class CategoricalRiskObserver:
         the type and name of a risk, specified as "type.name". Type is singular.
 
         """
-        self.risk_type, self.risk_name = split_risk_from_type(risk)
-        self.configuration_defaults = {f'{self.risk_name}_observer':
-                                           CategoricalRiskObserver.configuration_defaults['risk_observer']}
+        self.risk = RiskString(risk)
+        self.configuration_defaults = {
+            f'{self.risk.name}_observer': CategoricalRiskObserver.configuration_defaults['risk_observer']
+        }
 
     def setup(self, builder):
+        self.config = builder.configuration[f'{self.risk.name}_observer']
+        self.data = {}
+
         self.clock = builder.time.clock()
-        self.population_view = builder.population.get_view(['alive', 'age'])
+
+        self.categories = builder.data.load(f'{self.risk}.categories')
         self.age_bins = builder.data.load('population.age_bins')
         exit_age = builder.configuration.population.exit_age
-
         if exit_age:
             self.age_bins = self.age_bins[self.age_bins.age_group_start < exit_age]
             self.age_bins.loc[self.age_bins.age_group_end > exit_age, 'age_group_end'] = exit_age
 
-        self.sample_date_config = builder.configuration.observer.risk.sample_date
-        self.exposure = builder.value.get_value(f'{self.risk_name}.exposure')
-        self.categories = builder.data.load(f'{self.risk_type}.{self.risk_name}.categories')
-        self.data = pd.DataFrame()
+        self.population_view = builder.population.get_view(['alive', 'age'], query='alive == "alive"')
 
+        self.exposure = builder.value.get_value(f'{self.risk.name}.exposure')
         builder.value.register_value_modifier('metrics', self.metrics)
+
         builder.event.register_listener('collect_metrics', self.on_collect_metrics)
 
     def on_collect_metrics(self, event):
         pop = self.population_view.get(event.index)
-        pop = pop[pop.alive == 'alive']
-        current_year = event.time.year
-        sample_date = pd.datetime(current_year, self.sample_date_config.month, self.sample_date_config.day)
 
-        if self.clock() <= sample_date < event.time:
-            frame_dict = {f'{self.risk_name}_{cat}': 0 for cat in self.categories}
-            exposure_proportion = pd.DataFrame(frame_dict, index=self.age_bins.index)
-            exposure_proportion['year'] = current_year
-            exposure_proportion['alive_simulants_in_age_group'] = 0
+        if self.should_sample(event.time):
+            sample = self.generate_sampling_frame()
+            exposure = self.exposure(pop.index)
+
             for group, age_group in self.age_bins.iterrows():
                 start, end = age_group.age_group_start, age_group.age_group_end
                 in_group = pop[(pop.age >= start) & (pop.age < end)]
-                exposure_in_group = self.exposure(in_group.index).value_counts()
-                exposure_proportion.loc[group, 'alive_simulants_in_age_group'] = len(in_group)
-                for cat, count in exposure_in_group.iteritems():
-                    exposure_proportion.loc[group, f'{self.risk_name}_{cat}'] = count/len(in_group)
-            self.data = self.data.append(exposure_proportion)
+                sample.loc[group] = exposure.loc[in_group.index].value_counts()
+
+            self.data[self.clock().year] = sample
+
+    def should_sample(self, event_time: pd.Timestamp):
+        sample_date = pd.Timestamp(event_time.year, self.config.sample_date.month, self.config.sample_date.day)
+        return self.clock() <= sample_date < event_time
+
+    def generate_sampling_frame(self):
+        sample = pd.DataFrame({f'{cat}': 0 for cat in self.categories}, index=self.age_bins.index)
+        return sample
 
     def metrics(self, index, metrics):
-        result = self.age_bins.join(self.data).drop(['age_group_start', 'age_group_end'], axis=1)
-        result = pd.melt(result, id_vars=['age_group_name', 'year'])
-        for _, row in result.iterrows():
-            metrics[f'age_group_{row.age_group_name.replace(" ", "_")}_year_{row.year}_{row.variable}'] = row.value
-
+        for age_id, age_group in self.age_bins.iterrows():
+            age_group_name = age_group.age_group_name.replace(" ", "_").lower()
+            for year, sample in self.data.items():
+                for category in sample.columns:
+                    label = f'{self.risk.name}_{category}_exposed_in_{year}_among_{age_group_name}'
+                    metrics[label] = sample.loc[age_id, category]
         return metrics
-
