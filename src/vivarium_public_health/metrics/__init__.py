04a83c0a53ee4ce9fe635d1753109e3587687e95
James Collins <collijk@uw.edu>
2019-02-22 15:20:57 -0800

    Add Treatment observer to old code

--- src/vivarium_public_health/metrics/__init__.py
+++ src/vivarium_public_health/metrics/__init__.py
@@ -2,3 +2,5 @@ from .inspector import Inspector
 from .sample_history import SampleHistory
 from .disability import Disability
 from .mortality import MortalityObserver
+from .disease import DiseaseObserver
+from .treatment import TreatmentObserver
