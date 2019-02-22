c2542a0b7df79caf3f77d5472c7a8b9c9f6eeb8a
James Collins <collijk@uw.edu>
2019-02-22 13:27:27 -0800

    Standardize treatment component utility usage

--- src/vivarium_public_health/metrics/treatment.py
+++ src/vivarium_public_health/metrics/treatment.py
@@ -1,4 +1,6 @@
-import pandas as pd
+from collections import defaultdict
+
+from .utilities import get_age_bins, get_output_template, get_group_counts
 
 
 class TreatmentObserver:
@@ -14,39 +16,54 @@ class TreatmentObserver:
     """
 
     configuration_defaults = {
-        'metrics':{
-            'treatment_observer':{
-                'doses': ['first', 'second', 'booster', 'catchup']
+        'metrics': {
+            'treatment_observer': {
+                'by_age': False,
+                'by_year': False,
+                'by_sex': False,
             }
         }
     }
 
     def __init__(self, treatment: str):
         self.treatment = treatment
-        self.name = f'{self.treatment}_treatment_observer'
-        self.configuration_defaults = {'metrics': {
-            f'{self.treatment}_observer': TreatmentObserver.configuration_defaults['metrics']['treatment_observer']
-        }}
+        self.name = f'{self.treatment}_observer'
+        self.configuration_defaults = {
+            'metrics': {self.name: TreatmentObserver.configuration_defaults['metrics']['treatment_observer']}
+        }
 
     def setup(self, builder):
-        columns_required = ['tracked', 'alive', f'{self.treatment}_current_dose_event_time',
-                            f'{self.treatment}_current_dose' ]
-        self.population_view = builder.population.get_view(columns_required)
-        self.doses = self.configuration_defaults['metrics'][f'{self.treatment}_observer']['doses']
-        years = range(builder.configuration.time.start.year, builder.configuration.time.end.year + 1)
-        self.data = pd.DataFrame({f'{dose}_counts': 0 for dose in self.doses}, index=years)
+        self.config = builder.configuration['metrics'][self.name]
+        self.doses = builder.configuration[self.treatment]['doses']
+
+        self.output_template = get_output_template(**self.config.to_dict())
+
+        self.age_bins = get_age_bins(builder)
+        self.counts = defaultdict(int)
+
+        columns_required = ['alive', f'{self.treatment}_current_dose_event_time', f'{self.treatment}_current_dose']
+        if self.config.by_age:
+            columns_required += ['age']
+        if self.config.by_sex:
+            columns_required += ['sex']
+
+        self.population_view = builder.population.get_view(columns_required, query='alive == "alive"')
+
         builder.value.register_value_modifier('metrics', self.metrics)
-        builder.event.register_listener('collect_metrics', self.on_collect_metrics)
+        builder.event.register_listener('on_collect_metrics', self.on_collect_metrics)
 
     def on_collect_metrics(self, event):
         pop = self.population_view.get(event.index)
+
+        base_key = self.output_template.safe_substitute(year=event.time.year)
+
         for dose in self.doses:
-            pop_received_dose = pop[(pop[f'{self.treatment}_current_dose'] == dose) &
-                                    (pop[f'{self.treatment}_current_dose_event_time'] == event.time)]
-            self.data.loc[event.time.year, f'{dose}_counts'] += len(pop_received_dose)
+            base_filter = (f'{self.treatment}_current_dose == {dose} and '
+                           f'{self.treatment}_current_dose_event_time == {event.time}')
+            group_counts = get_group_counts(pop, base_filter, base_key, self.config.to_dict(), self.age_bins)
+            key = base_key.safe_substitute(measure=f'{self.treatment}_{dose}_count')
+            self.counts[key] += group_counts
 
     def metrics(self, index, metrics):
-        for label, counts in self.data.iteritems():
-            for year in counts.index:
-                metrics[f'{self.treatment}_{label}_in_{year}'] = counts.loc[year]
+        metrics.update(self.counts)
         return metrics
