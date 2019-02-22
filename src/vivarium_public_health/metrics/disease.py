35afe8535f3eccc3ee55a47c4b14d89e8d54db3b
James Collins <collijk@uw.edu>
2019-02-22 11:42:20 -0800

    Add disease observer code

---
+++ src/vivarium_public_health/metrics/disease.py
@@ -0,0 +1,71 @@
+from collections import defaultdict
+
+from .utilities import get_age_bins, get_output_template, to_years, get_group_counts
+
+
+class DiseaseObserver:
+    """Observes disease counts and person time for a single cause.
+    By default, this observer computes aggregate susceptible person time
+    and counts of disease cases over the entire simulation.  It can be
+    configured to bin these into age_groups, sexes, and years by setting
+    the ``by_age``, ``by_sex``, and ``by_year`` flags, respectively.
+    """
+    configuration_defaults = {
+        'disease_observer': {
+            'by_age': False,
+            'by_year': False,
+            'by_sex': False,
+        }
+    }
+
+    def __init__(self, disease: str):
+        self.disease = disease
+        self.name = f'{self.disease}_observer'
+        self.configuration_defaults = {self.name: DiseaseObserver.configuration_defaults['disease_observer']}
+
+    def setup(self, builder):
+        self.config = builder.configuration[self.name]
+
+        self.clock = builder.time.clock()
+
+        self.output_template = get_output_template(**self.config.to_dict())
+
+        self.age_bins = get_age_bins(builder)
+        self.counts = defaultdict(int)
+        self.person_time = defaultdict(float)
+
+        columns_required = ['alive', f'{self.disease}']
+        if self.config.by_age:
+            columns_required += ['age']
+        if self.config.by_sex:
+            columns_required += ['sex']
+
+        self.population_view = builder.population.get_view(columns_required, query='alive == "alive"')
+
+        builder.value.register_value_modifier('metrics', self.metrics)
+        # FIXME: The state table is modified before the clock advances.
+        # In order to get an accurate representation of person time and disease
+        # counts, we need to look at the state table before anything happens.
+        builder.event.register_listener('time_step__prepare', self.on_time_step_prepare)
+
+    def on_time_step_prepare(self, event):
+        pop = self.population_view.get(event.index)
+
+        # Ignoring the edge case where the step spans a new year.
+        # Accrue all counts and time to the current year.
+        base_key = self.output_template.safe_substitute(year=self.clock().year)
+        base_filter = f'{self.disease} == susceptible_to_{self.disease}'
+
+        group_counts = get_group_counts(pop, base_filter, base_key, self.config.to_dict(), self.age_bins)
+
+        for key, count in group_counts.items():
+            count_key = key.safe_substitute(measure=f'{self.disease}_counts')
+            person_time_key = key.safe_substitute(measure=f'{self.disease}_susceptible_person_time')
+
+            self.counts[count_key] += count
+            self.person_time[person_time_key] += count * to_years(event.step_size)
+
+    def metrics(self, index, metrics):
+        metrics.update(self.counts)
+        metrics.update(self.person_time)
+        return metrics
