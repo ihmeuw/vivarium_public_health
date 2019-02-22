c0007a81e00c40de756c0ab39d750bf2603b35a6
James Collins <collijk@uw.edu>
2019-02-22 08:58:43 -0800

    Update disease observer

--- src/vivarium_public_health/metrics/disease.py
+++ src/vivarium_public_health/metrics/disease.py
@@ -1,5 +1,9 @@
+from collections import defaultdict
+
 import pandas as pd
-from .utilities import get_age_bins
+
+from .utilities import get_age_bins, get_output_template
+from .mortality import to_years
 
 
 class DiseaseObserver:
@@ -10,43 +14,82 @@ class DiseaseObserver:
     year.
 
     """
+
+    configuration_defaults = {
+        'disease_observer': {
+            'by_age': False,
+            'by_year': False,
+            'by_sex': False,
+        }
+    }
+
     def __init__(self, disease: str):
         self.disease = disease
-        self.name = f'{self.disease}_disease_observer'
+        self.name = f'{self.disease}_observer'
+        self.configuration_defaults = {self.name: DiseaseObserver.configuration_defaults['disease_observer']}
 
     def setup(self, builder):
-        self.age_bins = get_age_bins(builder)
-        columns_required = ['tracked', 'alive', 'age', 'sex', f'{self.disease}', f'{self.disease}_event_time']
-        self.population_view = builder.population.get_view(columns_required)
+        self.config = builder.configuration[self.name]
 
-        years = range(builder.configuration.time.start.year, builder.configuration.time.end.year+1)
-        frame_index = pd.MultiIndex.from_product([self.age_bins.index, years, ['Male', 'Female']],
-                                                 names=['age_group', 'year', 'sex'])
-        self.frame = pd.DataFrame({f'{self.disease}_cases': 0, 'susceptible_person_time': 0}, index=frame_index)
+        self.clock = builder.time.clock()
+
+        self.output_template = get_output_template(**self.config.to_dict())
+
+        self.age_bins = get_age_bins(builder)
+        self.counts = defaultdict(int)
+        self.person_time = defaultdict(float)
+
+        columns_required = ['alive', 'exit_time', f'{self.disease}', f'{self.disease}_event_time']
+        if self.config.by_age:
+            columns_required += ['age']
+        if self.config.by_sex:
+            columns_required += ['sex']
+
+        self.population_view = builder.population.get_view(columns_required, query='alive == "alive"')
 
         builder.value.register_value_modifier('metrics', self.metrics)
-        builder.event.register_listener('collect_metrics', self.on_collect_metrics)
+        # FIXME: The state table is modified before the clock advances.
+        # In order to get an accurate representation of person time and disease
+        # counts, we need to look at the state table before anything happens.
+        builder.event.register_listener('time_step__prepare', self.on_time_step_prepare)
 
-    def on_collect_metrics(self, event):
+    def on_time_step_prepare(self, event):
         pop = self.population_view.get(event.index)
-        pop = pop[pop.alive == 'alive']
-        for group, age_group in self.age_bins.iterrows():
+
+        # Ignoring the edge case where the step spans a new year.
+        # Accrue all counts and time to the current year.
+        key = self.output_template.safe_substitute(year=self.clock().year)
+        count_key = key.safe_substitute(measure=f'{self.disease}_counts')
+        person_time_key = key.safe_substitute(measure=f'{self.disease}_susceptible_person_time')
+
+        filter_string = f'{self.disease} == susceptible_to_{self.disease}'
+
+        if self.config.by_age:
+            ages = self.age_bins.iterrows()
+            filter_string += ' and ({age_group_start} <= age) and (age < {age_group_end})'
+        else:
+            ages = [('all_ages', pd.Series({'age_group_start': None, 'age_group_end': None}))]
+
+        if self.config.by_sex:
+            sexes = ['Male', 'Female']
+            filter_string += ' and sex == {sex}'
+        else:
+            sexes = ['Both']
+
+        for group, age_group in ages:
             start, end = age_group.age_group_start, age_group.age_group_end
-            for sex in ['Male', 'Female']:
-                in_age_group = pop[(pop.age >= start) & (pop.age < end) & (pop.sex == sex)]
-                new_disease_cases = in_age_group[in_age_group[f'{self.disease}_event_time'] == event.time]
-                susceptible_pop = in_age_group[in_age_group[f'{self.disease}'] == f'susceptible_to_{self.disease}']
-                self.frame.loc[(group, event.time.year, sex), f'{self.disease}_cases'] += len(new_disease_cases)
-                self.frame.loc[(group, event.time.year, sex), 'susceptible_person_time'] += \
-                    (event.step_size / pd.Timedelta(days=365.25)) * len(susceptible_pop)
+            for sex in sexes:
+                filter_kwargs = {'age_group_start': start, 'age_group_end': end, 'sex': sex}
+                group_count_key = count_key.safe_substitute(**filter_kwargs)
+                group_person_time_key = person_time_key.safe_substitute(**filter_kwargs)
+                group_filter = filter_string.format(**filter_kwargs)
+
+                in_group = pop.query(group_filter)
+
+                self.counts[group_count_key] += len(in_group)
+                self.person_time[group_person_time_key] += len(in_group) * to_years(event.step_size)
 
     def metrics(self, index, metrics):
-        data = self.frame.to_dict(orient='index')
-        for key, value in data.items():
-            age_id, year, sex = key
-            age_group_name = self.age_bins.age_group_name.loc[age_id].replace(" ", "_").lower()
-            case_label = f'{self.disease}_cases_for_{sex}_in_age_group_{age_group_name}_in_year_{year}'
-            person_time_label = f'{self.disease}_susceptible_person_time_{sex}_in_age_group_{age_group_name}_in_{year}'
-            metrics[case_label] = value[f'{self.disease}_cases']
-            metrics[person_time_label] = value['susceptible_person_time']
+        metrics.update(self.counts)
+        metrics.update(self.person_time)
         return metrics
--- src/vivarium_public_health/metrics/utilities.py
+++ src/vivarium_public_health/metrics/utilities.py
@@ -1,3 +1,4 @@
+from string import Template
 
 def get_age_bins(builder):
     age_bins = builder.data.load('population.age_bins')
@@ -6,3 +7,14 @@ def get_age_bins(builder):
         age_bins = age_bins[age_bins.age_group_start < exit_age]
         age_bins.loc[age_bins.age_group_end > exit_age, 'age_group_end'] = exit_age
     return age_bins
+
+
+def get_output_template(by_age, by_sex, by_year):
+    template = '{measure}'
+    if by_year:
+        template += '_in_{year}'
+    if by_sex:
+        template += '_among_{sex}'
+    if by_age:
+        template += 'in_age_group_{age_group}'
+    return Template(template)
