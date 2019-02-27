5357bd07065417457e947ed0aa7338aaf6d0e6cc
Michelle Park <hpark3@uw.edu>
2019-02-27 13:31:03 -0800

    put back the special disease test

--- src/vivarium_public_health/disease/__init__.py
+++ src/vivarium_public_health/disease/__init__.py
@@ -4,3 +4,4 @@ from .state import (DiseaseState, TransientDiseaseState, ExcessMortalityState,
 from .model import DiseaseModel
 from .models import (SI, SIR, SIS, SIS_fixed_duration,
                      NeonatalSWC_with_incidence, NeonatalSWC_without_incidence)
+from .special_disease import RiskAttributableDisease
