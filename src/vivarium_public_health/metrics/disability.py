084319e1e7f3a2af951d166432eb3fa429b41160
James Collins <collijk@uw.edu>
2019-03-14 11:17:46 -0700

    Backport observers and tests

--- src/vivarium_public_health/metrics/disability.py
+++ src/vivarium_public_health/metrics/disability.py
@@ -1,38 +1,89 @@
-import pandas as pd
+from collections import Counter
 
+import pandas as pd
 from vivarium.framework.values import list_combiner, joint_value_post_processor, rescale_post_processor
 
-
-def _disability_post_processor(value, step_size):
-    return rescale_post_processor(joint_value_post_processor(value, step_size), step_size)
+from vivarium_public_health.disease import DiseaseModel
+from .utilities import get_age_bins, get_years_lived_with_disability
 
 
 class Disability:
-    """Measures and assigns disability to the simulants."""
+    """Counts years lived with disability.
+    By default, this counts both aggregate and cause-specific years lived
+    with disability over the full course of the simulation. It can be
+    configured to bin the cause-specific YLDs into age groups, sexes, and years
+    by setting the ``by_age``, ``by_sex``, and ``by_year`` flags, respectively.
+    In the model specification, your configuration for this component should
+    be specified as, e.g.:
+    configuration:
+        metrics:
+            disability:
+                by_age: True
+                by_year: False
+                by_sex: True
+    """
+    configuration_defaults = {
+        'metrics': {
+            'disability': {
+                'by_age': False,
+                'by_year': False,
+                'by_sex': False,
+            }
+        }
+    }
+
     def __init__(self):
-        self.years_lived_with_disability = pd.Series([])
+        self.name = 'disability_observer'
 
     def setup(self, builder):
+        self.config = builder.configuration.metrics.disability
+        self.age_bins = get_age_bins(builder)
+        self.clock = builder.time.clock()
+        self.step_size = builder.time.step_size()
+        self.causes = [c.state_column for c in builder.components.get_components(DiseaseModel)]
+        self.years_lived_with_disability = Counter()
+        self.disability_weight_pipelines = {cause: builder.value.get_value(f'{cause}.disability_weight')
+                                            for cause in self.causes}
+
         self.disability_weight = builder.value.register_value_producer(
             'disability_weight',
             source=lambda index: [pd.Series(0.0, index=index)],
             preferred_combiner=list_combiner,
             preferred_post_processor=_disability_post_processor)
-        builder.value.register_value_modifier('metrics', modifier=self.metrics)
 
-        self.population_view = builder.population.get_view(['years_lived_with_disability'])
+        columns_required = ['tracked', 'alive', 'years_lived_with_disability']
+        if self.config.by_age:
+            columns_required += ['age']
+        if self.config.by_sex:
+            columns_required += ['sex']
+        self.population_view = builder.population.get_view(columns_required)
         builder.population.initializes_simulants(self.initialize_disability,
                                                  creates_columns=['years_lived_with_disability'])
-        builder.event.register_listener('collect_metrics', self.calculate_ylds)
+        # FIXME: The state table is modified before the clock advances.
+        # In order to get an accurate representation of person time we need to look at
+        # the state table before anything happens.
+        builder.event.register_listener('time_step__prepare', self.on_time_step_prepare)
+        builder.value.register_value_modifier('metrics', modifier=self.metrics)
 
     def initialize_disability(self, pop_data):
-        self.population_view.update(pd.Series(0., index=pop_data.index))
+        self.population_view.update(pd.Series(0., index=pop_data.index, name='years_lived_with_disability'))
 
-    def calculate_ylds(self, event):
-        disability = self.population_view.get(event.index)['years_lived_with_disability']
-        disability += self.disability_weight(event.index)
-        self.population_view.update(disability)
+    def on_time_step_prepare(self, event):
+        pop = self.population_view.get(event.index, query='tracked == True and alive == "alive"')
+        ylds_this_step = get_years_lived_with_disability(pop, self.config.to_dict(),
+                                                         self.clock().year, self.step_size(),
+                                                         self.age_bins, self.disability_weight_pipelines, self.causes)
+        self.years_lived_with_disability.update(ylds_this_step)
+
+        pop.loc[:, 'years_lived_with_disability'] += self.disability_weight(pop.index)
+        self.population_view.update(pop)
 
     def metrics(self, index, metrics):
-        metrics['years_lived_with_disability'] = self.population_view.get(index)['years_lived_with_disability'].sum()
+        total_ylds = self.population_view.get(index)['years_lived_with_disability'].sum()
+        metrics['years_lived_with_disability'] = total_ylds
+        metrics.update(self.years_lived_with_disability)
         return metrics
+
+
+def _disability_post_processor(value, step_size):
+    return rescale_post_processor(joint_value_post_processor(value, step_size), step_size)
--- src/vivarium_public_health/metrics/disease.py
+++ src/vivarium_public_health/metrics/disease.py
@@ -1,26 +1,22 @@
-from collections import defaultdict
+from collections import Counter
 
-from .utilities import get_age_bins, get_output_template, to_years, get_group_counts
+from .utilities import get_age_bins, get_disease_event_counts, get_susceptible_person_time
 
 
 class DiseaseObserver:
     """Observes disease counts and person time for a single cause.
-
     By default, this observer computes aggregate susceptible person time
     and counts of disease cases over the entire simulation.  It can be
     configured to bin these into age_groups, sexes, and years by setting
     the ``by_age``, ``by_sex``, and ``by_year`` flags, respectively.
-
     In the model specification, your configuration for this component should
     be specified as, e.g.:
-
     configuration:
         metrics:
             YOUR_DISEASE_NAME:
                 by_age: True
                 by_year: False
                 by_sex: True
-
     """
     configuration_defaults = {
         'metrics': {
@@ -41,21 +37,16 @@ class DiseaseObserver:
 
     def setup(self, builder):
         self.config = builder.configuration['metrics'][self.name]
-
         self.clock = builder.time.clock()
-
-        self.output_template = get_output_template(**self.config.to_dict())
-
         self.age_bins = get_age_bins(builder)
-        self.counts = defaultdict(int)
-        self.person_time = defaultdict(float)
+        self.counts = Counter()
+        self.person_time = Counter()
 
         columns_required = ['alive', f'{self.disease}', f'{self.disease}_event_time']
         if self.config.by_age:
             columns_required += ['age']
         if self.config.by_sex:
             columns_required += ['sex']
-
         self.population_view = builder.population.get_view(columns_required)
 
         builder.value.register_value_modifier('metrics', self.metrics)
@@ -66,30 +57,18 @@ class DiseaseObserver:
         builder.event.register_listener('on_collect_metrics', self.on_collect_metrics)
 
     def on_time_step_prepare(self, event):
-        pop = self.population_view.get(event.index, query='alive == "alive"')
-
+        pop = self.population_view.get(event.index)
         # Ignoring the edge case where the step spans a new year.
         # Accrue all counts and time to the current year.
-        base_key = self.output_template.safe_substitute(year=self.clock().year)
-        base_filter = f'{self.disease} == susceptible_to_{self.disease}'
-
-        group_counts = get_group_counts(pop, base_filter, base_key, self.config.to_dict(), self.age_bins)
-
-        for key, count in group_counts.items():
-            person_time_key = key.safe_substitute(measure=f'{self.disease}_susceptible_person_time')
-            self.person_time[person_time_key] += count * to_years(event.step_size)
+        person_time_this_step = get_susceptible_person_time(pop, self.config.to_dict(), self.disease,
+                                                            self.clock().year, event.step_size, self.age_bins)
+        self.person_time.update(person_time_this_step)
 
     def on_collect_metrics(self, event):
         pop = self.population_view.get(event.index)
-
-        base_key = self.output_template.safe_substitute(year=event.time.year)
-        base_filter = f'{self.disease}_event_time == {event.time}'
-
-        group_counts = get_group_counts(pop, base_filter, base_key, self.config.to_dict(), self.age_bins)
-
-        for key, count in group_counts.items():
-            count_key = key.safe_substitute(measure=f'{self.disease}_counts')
-            self.counts[count_key] += count
+        disease_events_this_step = get_disease_event_counts(pop, self.config.to_dict(), self.disease,
+                                                            event.time, self.age_bins)
+        self.counts.update(disease_events_this_step)
 
     def metrics(self, index, metrics):
         metrics.update(self.counts)
