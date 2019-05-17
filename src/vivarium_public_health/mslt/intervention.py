24ebc442235f1370324cbc3f050d52831296f0bf
Rob Moss <robm.dev@gmail.com>
2019-05-17 13:39:36 +1000

    Fix the tobacco eradication intervention
    Moving people directly from currently-smoking to 0-years post-cessation
    produced undesired results, because the tobacco component then moves
    these people from 0-years post-cessation to 1-year post-cessation in the
    same time-step.

--- src/vivarium_public_health/mslt/intervention.py
+++ src/vivarium_public_health/mslt/intervention.py
@@ -159,32 +159,26 @@ class TobaccoEradication:
     def setup(self, builder):
         self.year = builder.configuration['tobacco_eradication'].year
         self.clock = builder.time.clock()
-        rate_name = '{}_intervention.incidence'.format(self.exposure)
-        builder.value.register_value_modifier(rate_name, self.adjust_rate)
+        inc_rate_name = '{}_intervention.incidence'.format(self.exposure)
+        builder.value.register_value_modifier(inc_rate_name,
+                                              self.adjust_inc_rate)
+        rem_rate_name = '{}_intervention.remission'.format(self.exposure)
+        builder.value.register_value_modifier(rem_rate_name,
+                                              self.adjust_rem_rate)
         self.prevalence_col = '{}_intervention.yes'.format(self.exposure)
         self.remission_col = '{}_intervention.0'.format(self.exposure)
         view_columns = [self.prevalence_col, self.remission_col]
         self.population_view = builder.population.get_view(view_columns)
-        # Add a handler to cease all current tobacco use.
-        builder.event.register_listener('time_step__prepare',
-                                        self.stop_current_use)
 
-    def adjust_rate(self, index, rates):
+    def adjust_inc_rate(self, index, rates):
         this_year = self.clock().year
         if this_year >= self.year:
             return 0.0 * rates
         else:
             return rates
 
-    def stop_current_use(self, event):
-        """
-        Make all current tobacco users cease their tobacco use.
-        """
+    def adjust_rem_rate(self, index, rates):
         this_year = self.clock().year
-        if this_year != self.year:
-            return
-        pop = self.population_view.get(event.index)
-        pop.loc[:, self.remission_col] = (pop.loc[:, self.remission_col] +
-                                          pop.loc[:, self.prevalence_col])
-        pop.loc[:, self.prevalence_col] = 0.0
-        self.population_view.update(pop)
+        if this_year >= self.year:
+            rates[:] = 1.0
+        return rates
