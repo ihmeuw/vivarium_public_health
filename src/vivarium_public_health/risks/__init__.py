e8e7226ccbca3fd4643a55a6a172b8d640c77029
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-04-27 14:27:24 -0700

    add components to the correct __init__.py file

--- src/vivarium_public_health/risks/__init__.py
+++ src/vivarium_public_health/risks/__init__.py
@@ -2,6 +2,7 @@ from .base_risk import Risk
 from .distributions import get_distribution
 from .effect import RiskEffect
 from .implementations.low_birth_weight_and_short_gestation import (
+    LBWSGDistribution,
     LBWSGRisk,
     LBWSGRiskEffect,
 )
