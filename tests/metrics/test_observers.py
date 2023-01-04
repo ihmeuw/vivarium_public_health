55be49d44db114261ab7ab6d247567a0b37c7611
Matthew Kappel <mkappel@uw.edu>
2023-01-03 16:17:29 -0800

    Initial implementation and test for disability observer

---
+++ tests/metrics/test_observers.py
@@ -0,0 +1,28 @@
+from vivarium_public_health.disease import DiseaseState, RiskAttributableDisease
+from vivarium_public_health.metrics.disability import DisabilityObserver
+
+
+def test_disability_observer_setup(mocker):
+    observer = DisabilityObserver()
+    builder = mocker.Mock()
+    builder.results.register_observation = mocker.Mock()
+    builder.components.get_components_by_type = lambda n: []
+
+    builder.results.register_observation.assert_not_called()
+    observer.setup(builder)
+    builder.results.register_observation.assert_called_once_with(
+        name="ylds_due_to_all_causes",
+        pop_filter='tracked == True and alive == "alive"',
+        aggregator_sources=[str(observer.disability_weight)],  # XXX is this right?
+        aggregator=observer._disability_weight_aggregator,  # XXX is this right?
+        requires_columns=["alive"],
+        requires_values=["disability_weight"],
+        additional_stratifications=observer.config.include,
+        excluded_stratifications=observer.config.exclude,
+        when="time_step__prepare",
+    )
+    assert DiseaseState in observer.disease_classes
+    assert RiskAttributableDisease in observer.disease_classes
+
+
+# Test _disability_weight_aggregator?
