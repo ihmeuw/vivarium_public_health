113aa89f146c5f2a5e110ba2734fd86de4e615fd
Matthew Kappel <mkappel@uw.edu>
2023-01-24 21:52:20 -0800

    Fix duplicated previous column name variable

--- src/vivarium_public_health/metrics/disease.py
+++ src/vivarium_public_health/metrics/disease.py
@@ -66,13 +66,12 @@ class DiseaseObserver:
         self.config = builder.configuration.stratification[self.disease]
 
         disease_model = builder.components.get_component(f"disease_model.{self.disease}")
-        self.previous_state_column = f"previous_{self.disease}"
 
         builder.population.initializes_simulants(
-            self.on_initialize_simulants, creates_columns=[self.previous_state_column]
+            self.on_initialize_simulants, creates_columns=[self.previous_state_column_name]
         )
 
-        columns_required = [self.disease, self.previous_state_column]
+        columns_required = [self.disease, self.previous_state_column_name]
         self.population_view = builder.population.get_view(columns_required)
 
         builder.event.register_listener("time_step__prepare", self.on_time_step_prepare)
@@ -90,13 +89,13 @@ class DiseaseObserver:
 
         for transition in disease_model.transition_names:
             filter_string = (
-                f'{self.previous_state_column} == "{transition.from_state}" '
+                f'{self.previous_state_column_name} == "{transition.from_state}" '
                 f'and {self.disease} == "{transition.to_state}"'
             )
             builder.results.register_observation(
                 name=f"{transition}_event_count",
                 pop_filter=filter_string,
-                requires_columns=[self.previous_state_column, self.disease],
+                requires_columns=[self.previous_state_column_name, self.disease],
                 additional_stratifications=self.config.include,
                 excluded_stratifications=self.config.exclude,
                 when="collect_metrics",
@@ -114,11 +113,11 @@ class DiseaseObserver:
 
     def on_initialize_simulants(self, pop_data):
         self.population_view.update(
-            pd.Series("", index=pop_data.index, name=self.previous_state_column)
+            pd.Series("", index=pop_data.index, name=self.previous_state_column_name)
         )
 
     def on_time_step_prepare(self, event) -> None:
         # This enables tracking of transitions between states
         prior_state_pop = self.population_view.get(event.index)
-        prior_state_pop[self.previous_state_column] = prior_state_pop[self.disease]
+        prior_state_pop[self.previous_state_column_name] = prior_state_pop[self.disease]
         self.population_view.update(prior_state_pop)
