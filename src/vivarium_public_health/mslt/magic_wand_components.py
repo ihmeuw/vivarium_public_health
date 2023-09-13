6e3fb65820451b4d6d0277c7fcdca07b5f16bd35
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2023-09-13 17:59:36 +0000

    refactor MSLT components (#350)
    * refactor mortality and fertility components as Components
    
    * refactor MSLT DelayedRisk as a Component
    
    * refactor MSLT disease components as Components
    
    * refactor MSLT intervention components as Components
    
    * refactor MSLT magic wand components as Components
    
    * refactor MSLT observers as Components
    
    * refactor MSLT population components as Components

--- src/vivarium_public_health/mslt/magic_wand_components.py
+++ src/vivarium_public_health/mslt/magic_wand_components.py
@@ -7,86 +7,143 @@ This module contains tools for making crude adjustments to rates in
 multi-state lifetable simulations.
 
 """
+from typing import Any, Dict
+
+from vivarium import Component
+from vivarium.framework.engine import Builder
 
 
-class MortalityShift:
-    @property
-    def name(self):
-        return "mortality_shift"
+class MortalityShift(Component):
+    #####################
+    # Lifecycle methods #
+    #####################
 
-    def setup(self, builder):
+    def setup(self, builder: Builder) -> None:
+        super().setup(builder)
         builder.value.register_value_modifier("mortality_rate", self.mortality_adjustment)
+
+    ##################################
+    # Pipeline sources and modifiers #
+    ##################################
 
     def mortality_adjustment(self, index, rates):
         return rates * 0.5
 
 
-class YLDShift:
-    @property
-    def name(self):
-        return "yld_shift"
+class YLDShift(Component):
+    #####################
+    # Lifecycle methods #
+    #####################
 
-    def setup(self, builder):
+    def setup(self, builder: Builder) -> None:
+        super().setup(builder)
         builder.value.register_value_modifier("yld_rate", self.disability_adjustment)
+
+    ##################################
+    # Pipeline sources and modifiers #
+    ##################################
 
     def disability_adjustment(self, index, rates):
         return rates * 0.5
 
 
-class IncidenceShift:
-    def __init__(self, name):
-        self._name = name
+class IncidenceShift(Component):
+    #####################
+    # Lifecycle methods #
+    #####################
+    def __init__(self, disease: str):
+        super().__init__()
+        self.disease = disease
 
-    @property
-    def name(self):
-        return self._name
-
-    def setup(self, builder):
+    def setup(self, builder: Builder) -> None:
+        super().setup(builder)
         builder.value.register_value_modifier(
-            f"{self.name}_intervention.incidence", self.incidence_adjustment
+            f"{self.disease}_intervention.incidence", self.incidence_adjustment
         )
+
+    ##################################
+    # Pipeline sources and modifiers #
+    ##################################
 
     def incidence_adjustment(self, index, rates):
         return rates * 0.5
 
 
-class ModifyAcuteDiseaseYLD:
-    def __init__(self, name):
-        self._name = name
+class ModifyAcuteDiseaseYLD(Component):
+    ##############
+    # Properties #
+    ##############
 
     @property
-    def name(self):
-        return self._name
+    def configuration_defaults(self) -> Dict[str, Any]:
+        return {
+            "intervention": {
+                self.disease: {
+                    "yld_scale": 1.0,
+                },
+            }
+        }
 
-    def setup(self, builder):
+    #####################
+    # Lifecycle methods #
+    #####################
+    def __init__(self, disease: str):
+        super().__init__()
+        self.disease = disease
+
+    def setup(self, builder: Builder) -> None:
+        super().setup(builder)
         self.config = builder.configuration
-        self.scale = self.config.intervention[self.name].yld_scale
+        self.scale = self.config.intervention[self.disease].yld_scale
         if self.scale < 0:
             raise ValueError(f"Invalid YLD scale: {self.scale}")
         builder.value.register_value_modifier(
-            f"{self.name}_intervention.yld_rate", self.disability_adjustment
+            f"{self.disease}_intervention.yld_rate", self.disability_adjustment
         )
+
+    ##################################
+    # Pipeline sources and modifiers #
+    ##################################
 
     def disability_adjustment(self, index, rates):
         return rates * self.scale
 
 
-class ModifyAcuteDiseaseMortality:
-    def __init__(self, name):
-        self._name = name
+class ModifyAcuteDiseaseMortality(Component):
+    ##############
+    # Properties #
+    ##############
 
     @property
-    def name(self):
-        return self._name
+    def configuration_defaults(self) -> Dict[str, Any]:
+        return {
+            "intervention": {
+                self.disease: {
+                    "mortality_scale": 1.0,
+                },
+            }
+        }
 
-    def setup(self, builder):
+    #####################
+    # Lifecycle methods #
+    #####################
+    def __init__(self, disease: str):
+        super().__init__()
+        self.disease = disease
+
+    def setup(self, builder: Builder) -> None:
+        super().setup(builder)
         self.config = builder.configuration
-        self.scale = self.config.intervention[self.name].mortality_scale
+        self.scale = self.config.intervention[self.disease].mortality_scale
         if self.scale < 0:
             raise ValueError(f"Invalid mortality scale: {self.scale}")
         builder.value.register_value_modifier(
-            f"{self.name}_intervention.excess_mortality", self.mortality_adjustment
+            f"{self.disease}_intervention.excess_mortality", self.mortality_adjustment
         )
+
+    ##################################
+    # Pipeline sources and modifiers #
+    ##################################
 
     def mortality_adjustment(self, index, rates):
         return rates * self.scale
