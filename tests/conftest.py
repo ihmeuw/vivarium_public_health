1ee7f026d13ad924db30f587fb401f69f7a423a3
albrja <albrja@uw.edu>
2023-09-22 13:30:37 -0700

    Add runslow in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,24 @@ from vivarium.config_tree import ConfigTree
 from vivarium.framework.configuration import build_simulation_configuration
 
 
+def pytest_addoption(parser):
+    parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
+
+
 @pytest.fixture(scope="function")
 def base_config():
     config = build_simulation_configuration()
