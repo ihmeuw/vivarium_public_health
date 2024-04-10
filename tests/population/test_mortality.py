0b3d5418faba2c4737daacd32066606f0bfae48c
albrja <albrja@uw.edu>
2024-04-10 16:03:27 -0700

    Remove unnecessary config update

--- tests/population/test_mortality.py
+++ tests/population/test_mortality.py
@@ -8,7 +8,9 @@ from vivarium_public_health.population import BasePopulation, Mortality
 from vivarium_public_health.testing.mock_artifact import MockArtifact
 
 
-def test_mortality_configuration(make_full_simulants, base_plugins, generate_population_mock):
+def test_mortality_default_lookup_configuration(
+    make_full_simulants, base_plugins, generate_population_mock
+):
     sims = make_full_simulants
     start_population_size = len(sims)
 
@@ -24,13 +26,6 @@ def test_mortality_configuration(make_full_simulants, base_plugins, generate_pop
             "population_size": start_population_size,
             "include_sex": "Male",
         },
-        "mortality": {
-            "lookup_tables": {
-                "all_cause_mortality_rate": Component.build_lookup_table_config(
-                    value=0.25,
-                ),
-            },
-        },
     }
     sim.configuration.update(override_config)
     sim.setup()
@@ -42,6 +37,6 @@ def test_mortality_configuration(make_full_simulants, base_plugins, generate_pop
     ]
 
     assert set(expected_lookup_table_keys) == set(lookup_tables.keys())
-    assert lookup_tables["all_cause_mortality_rate"].data == 0.25
+    assert (lookup_tables["all_cause_mortality_rate"].data["value"] == 0.0).all()
     assert lookup_tables["unmodeled_cause_specific_mortality_rate"].data == 0.0
     assert isinstance(lookup_tables["life_expectancy"], InterpolatedTable)
