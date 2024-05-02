450abc08ed0804766e261137b9ec99db20821675
albrja <37345113+albrja@users.noreply.github.com>
2024-05-02 13:46:03 -0700

    Mic-4976/Tests/mortality (#392)
    Mic-4976/Tests/mortality
    
    Adds tests for mortality
    - *Category*: Test
    - *JIRA issue*: https://jira.ihme.washington.edu/browse/MIC-4976
    
    Changes and notes
    -Adds tests for the mortality component 
    
    Testing
    All tests pass.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
     setup_requires = ["setuptools_scm"]
 
     test_requirements = [
+        "vivarium_testing_utils",
         "pytest",
         "pytest-mock",
         "hypothesis",
