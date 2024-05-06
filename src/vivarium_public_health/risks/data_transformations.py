bf600263402177d115abe2d11b39161ca0642eb6
albrja <albrja@uw.edu>
2024-05-06 15:25:00 -0700

    Remove item from list

--- src/vivarium_public_health/risks/data_transformations.py
+++ src/vivarium_public_health/risks/data_transformations.py
@@ -174,6 +174,7 @@ def get_exposure_distribution_weights(
         if np.any(weights["glnorm"]):
             raise NotImplementedError("glnorm distribution is not supported")
         weights = weights.drop(columns=["glnorm"])
+        distributions.remove("glnorm")
     return weights, distributions
 
 
