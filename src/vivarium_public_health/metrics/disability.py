1e47ca3a0474e578ad002afae7e9a5b48932941a
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2024-05-08 13:59:05 -0600

    Feature/sbachmei/mic 4981 5036 update disability observer (#400)

--- src/vivarium_public_health/metrics/disability.py
+++ src/vivarium_public_health/metrics/disability.py
@@ -7,18 +7,20 @@ This module contains tools for observing years lived with disability (YLDs)
 in the simulation.
 
 """
+
+from pathlib import Path
 from typing import List
 
 import pandas as pd
-from vivarium import Component
 from vivarium.framework.engine import Builder
+from vivarium.framework.results import METRICS_COLUMN, StratifiedObserver
 from vivarium.framework.values import Pipeline, list_combiner, union_post_processor
 
 from vivarium_public_health.disease import DiseaseState, RiskAttributableDisease
 from vivarium_public_health.utilities import to_years
 
 
-class DisabilityObserver(Component):
+class DisabilityObserver(StratifiedObserver):
     """Counts years lived with disability.
 
     By default, this counts both aggregate and cause-specific years lived
@@ -37,15 +39,6 @@ class DisabilityObserver(Component):
                     include:
                         - "sample_stratification"
     """
-
-    CONFIGURATION_DEFAULTS = {
-        "stratification": {
-            "disability": {
-                "exclude": [],
-                "include": [],
-            }
-        }
-    }
 
     ##############
     # Properties #
@@ -68,6 +61,12 @@ class DisabilityObserver(Component):
         self.config = builder.configuration.stratification.disability
         self.step_size = pd.Timedelta(days=builder.configuration.time.step_size)
         self.disability_weight = self.get_disability_weight_pipeline(builder)
+
+    #################
+    # Setup methods #
+    #################
+
+    def register_observations(self, builder: Builder) -> None:
         cause_states = builder.components.get_components_by_type(tuple(self.disease_classes))
 
         builder.results.register_observation(
@@ -80,6 +79,7 @@ class DisabilityObserver(Component):
             additional_stratifications=self.config.include,
             excluded_stratifications=self.config.exclude,
             when="time_step__prepare",
+            report=self.report,
         )
 
         for cause_state in cause_states:
@@ -96,11 +96,8 @@ class DisabilityObserver(Component):
                 additional_stratifications=self.config.include,
                 excluded_stratifications=self.config.exclude,
                 when="time_step__prepare",
+                report=self.report,
             )
-
-    #################
-    # Setup methods #
-    #################
 
     def get_disability_weight_pipeline(self, builder: Builder) -> Pipeline:
         return builder.value.register_value_producer(
@@ -116,3 +113,39 @@ class DisabilityObserver(Component):
 
     def disability_weight_aggregator(self, dw: pd.DataFrame) -> float:
         return (dw * to_years(self.step_size)).sum().squeeze()
+
+    ##################
+    # Report methods #
+    ##################
+
+    def report(
+        self,
+        measure: str,
+        results: pd.DataFrame,
+    ) -> None:
+        """Combine the measure-specific observer results and save to a single file."""
+        results_dir = Path(self.results_dir)
+        measure, cause = [s.strip("_") for s in measure.split("due_to")]
+        # Add extra cols
+        col_map = {
+            "measure": measure,
+            "cause": cause,
+            "random_seed": self.random_seed,
+            "input_draw": self.input_draw,
+        }
+        for col, val in col_map.items():
+            if val is not None:
+                results[col] = val
+        # Sort the columns such that the stratifications (index) are first
+        # and METRICS_COLUMN is last and sort the rows by the stratifications.
+        other_cols = [c for c in results.columns if c != METRICS_COLUMN]
+        results = results[other_cols + [METRICS_COLUMN]].sort_index().reset_index()
+
+        # Concat and save
+        results_file = results_dir / f"{measure}.csv"
+        if not results_file.exists():
+            results.to_csv(results_file, index=False)
+        else:
+            results.to_csv(
+                results_dir / f"{measure}.csv", index=False, mode="a", header=False
+            )
