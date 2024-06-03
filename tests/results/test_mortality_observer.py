10a059a93237cf7e9bf43d4f581f37279de8343e
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2024-06-03 09:43:53 -0600

    Feature/sbachmei/mic 5095 update for refactored reporting (#429)
    * update observers for new reporting approach
    
    * update 'format_results' method to 'formatter'
    
    * rename 'metrics' folder to 'results'

--- tests/results/test_mortality_observer.py
+++ tests/results/test_mortality_observer.py
@@ -1,21 +1,18 @@
 import itertools
 from collections import Counter
-from pathlib import Path
 
 import numpy as np
-import pandas as pd
 import pytest
 from vivarium import InteractiveContext
 from vivarium.testing_utilities import TestPopulation
 
 from tests.test_utilities import build_table_with_age
-from tests.test_utilities import finalize_sim_and_get_results
 from vivarium_public_health.disease import DiseaseModel, DiseaseState
 from vivarium_public_health.disease.state import SusceptibleState
-from vivarium_public_health.metrics import MortalityObserver
-from vivarium_public_health.metrics.reporters import COLUMNS
-from vivarium_public_health.metrics.stratification import ResultsStratifier
 from vivarium_public_health.population import Mortality
+from vivarium_public_health.results import MortalityObserver
+from vivarium_public_health.results.columns import COLUMNS
+from vivarium_public_health.results.stratification import ResultsStratifier
 
 
 def disease_with_excess_mortality(base_config, disease_name, emr_value) -> DiseaseModel:
@@ -46,10 +43,8 @@ def disease_with_excess_mortality(base_config, disease_name, emr_value) -> Disea
 
 
 @pytest.fixture()
-def simulation_after_one_step(base_config, base_plugins, tmpdir):
+def simulation_after_one_step(base_config, base_plugins):
     observer = MortalityObserver()
-    # Add the results dir since we didn't go through cli.py
-    base_config.update({"output_data": {"results_directory": str(tmpdir)}})
     flu = disease_with_excess_mortality(base_config, "flu", 10)
     mumps = disease_with_excess_mortality(base_config, "mumps", 20)
     # TODO: Add test against using a RiskAttributableDisease in addition to a DiseaseModel
@@ -90,10 +85,12 @@ def simulation_after_one_step(base_config, base_plugins, tmpdir):
     return simulation
 
 
-def get_expected_results(simulation, expected_values=Counter()):
-    """Get expected results given a simulation. If expected deaths, for example, are not provided, return
-    the counts of deaths in this time step. If expected deaths are provided, return the counts of deaths
-    in the Counter plus the counts for this time step."""
+def _get_expected_results(simulation, expected_values=Counter()):
+    """Get expected results given a simulation. If expected deaths, for example,
+    are not provided, return the counts of deaths in this time step. If expected
+    deaths are provided, return the counts of deaths in the Counter plus the counts
+    for this time step.
+    """
     pop = simulation.get_population()
 
     for cause in ["other_causes", "flu", "mumps"]:
@@ -115,7 +112,7 @@ def get_expected_results(simulation, expected_values=Counter()):
 
 def test_observation_registration(simulation_after_one_step):
     """Test that all expected observation stratifications appear in the results."""
-    results = finalize_sim_and_get_results(simulation_after_one_step, ["deaths", "ylls"])
+    results = simulation_after_one_step.get_results()
     deaths = results["deaths"]
     ylls = results["ylls"]
 
@@ -128,22 +125,19 @@ def test_observation_registration(simulation_after_one_step):
 
 def test_observation_correctness(simulation_after_one_step):
     """Test that deaths and YLLs appear as expected in the results."""
-    pop = simulation_after_one_step.get_population()
-    metrics = simulation_after_one_step.get_value("metrics")(pop.index)
-    expected = get_expected_results(simulation_after_one_step)
-    _assert_metric_correctness(metrics, expected)
+    results = simulation_after_one_step.get_results()
+    expected = _get_expected_results(simulation_after_one_step)
+    _assert_correctness(results, expected)
 
     # same test on second time step
     simulation_after_one_step.step()
-    expected = get_expected_results(simulation_after_one_step, expected)
-    _assert_metric_correctness(metrics, expected)
+    results = simulation_after_one_step.get_results()
+    expected = _get_expected_results(simulation_after_one_step, expected)
+    _assert_correctness(results, expected)
 
-    results = finalize_sim_and_get_results(simulation_after_one_step, ["deaths", "ylls"])
-    deaths = results["deaths"]
-    ylls = results["ylls"]
-
+    # Check for all expected columns and stratifications
     for measure in ["deaths", "ylls"]:
-        df = eval(measure)
+        df = results[measure]
 
         # Check columns
         assert set(df.columns) == set(
@@ -164,19 +158,8 @@ def test_observation_correctness(simulation_after_one_step):
         assert (df[COLUMNS.SEED] == 0).all()
         assert df[COLUMNS.DRAW].isna().all()
 
-        # Check values. We already checked correctness of pipeline, so let's compare to that.
-        metrics_df = (
-            metrics[measure].reset_index().rename(columns={"cause_of_death": COLUMNS.ENTITY})
-        )
-        metrics_df = metrics_df.loc[metrics_df[COLUMNS.ENTITY] != "not_dead"].reset_index(
-            drop=True
-        )
-        assert metrics_df.equals(df[["sex", COLUMNS.ENTITY, COLUMNS.VALUE]])
 
-
-def test_aggregation_configuration(base_config, base_plugins, tmpdir):
-    # Add the results dir since we didn't go through cli.py
-    base_config.update({"output_data": {"results_directory": str(tmpdir)}})
+def test_aggregation_configuration(base_config, base_plugins):
 
     observer = MortalityObserver()
     flu = disease_with_excess_mortality(base_config, "flu", 10)
@@ -215,7 +198,7 @@ def test_aggregation_configuration(base_config, base_plugins, tmpdir):
     aggregate_sim.setup()
     aggregate_sim.step()
 
-    results = finalize_sim_and_get_results(aggregate_sim, ["deaths", "ylls"])
+    results = aggregate_sim.get_results()
     deaths = results["deaths"]
     ylls = results["ylls"]
 
@@ -230,12 +213,14 @@ def test_aggregation_configuration(base_config, base_plugins, tmpdir):
 ##################
 
 
-def _assert_metric_correctness(metrics, expected):
+def _assert_correctness(results, expected):
 
     for observation in expected:
         measure = observation.split("_due_to_")[0].strip("MEASURE_")
         cause = observation.split("_due_to_")[1].split("_SEX_")[0]
         sex = observation.split("SEX_")[1]
-        assert np.isclose(
-            expected[observation], metrics[measure].loc[(sex, cause), "value"], rtol=0.001
-        )
+        df = results[measure]
+        assert (
+            df.loc[(df["sex"] == sex) & (df["entity"] == cause), "value"]
+            == expected[observation]
+        ).all()
