085b762e9c1ac28d402ceb20b0450ffc09018aaa
Hussain Jafari <hussainsjafari@gmail.com>
2024-08-07 13:58:56 -0700

    docstring and exposure check

--- src/vivarium_public_health/risks/effect.py
+++ src/vivarium_public_health/risks/effect.py
@@ -323,8 +323,19 @@ class RiskEffect(Component):
 
 
 class NonLogLinearRiskEffect(RiskEffect):
-    # TODO: update docstring
-    """Risk effect for exposure-parametrized relative risks."""
+    """A component to model the impact of an exposure-parametrized risk factor on
+    the target rate of some affected entity. This component will
+
+    1) read TMRED data from the artifact and define the TMREL
+    2) calculate the relative risk at TMREL by linearly interpolating over
+       relative risk data defined in the configuration
+    3) divide relative risk data from configuration by RR at TMREL
+       and clip to be greater than 1
+    4) build a LookupTable which returns the exposure and RR of the left and right edges
+       of the RR bin containing a simulant's exposure
+    5) use this LookupTable to modify the target pipeline by linearly interpolating
+       a simulant's RR value and multiplying it by the intended target rate
+    """
 
     ##############
     # Properties #
@@ -358,7 +369,10 @@ class NonLogLinearRiskEffect(RiskEffect):
 
         # check that rr_data has 1000 parameter values
         exposure_values = rr_data["parameter"].values
-        assert len(np.unique(exposure_values)) == 1000
+        if len(np.unique(exposure_values)) != 1000:
+            raise ValueError(
+                f"The parameter column in your {self.risk.name} relative risk data must contain 1000 values to be used in NonLogLinearRiskEffect."
+            )
 
         # and that these values are monotonically increasing within each demographic group
         demographic_cols = [
@@ -371,7 +385,10 @@ class NonLogLinearRiskEffect(RiskEffect):
         group_is_increasing = rr_data.groupby(demographic_cols).apply(
             values_are_monotonically_increasing
         )
-        assert group_is_increasing.all()
+        if not group_is_increasing.all():
+            raise ValueError(
+                "The parameter column in your relative risk data must be monotonically increasing to be used in NonLogLinearRiskEffect."
+            )
 
         def define_rr_intervals(df: pd.DataFrame) -> pd.DataFrame:
             # create new row for right-most exposure bin (RR is same as max RR)
