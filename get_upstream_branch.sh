dbb6df4cae2e93749e2be0a008a8468b395d80b7
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-08-19 13:55:57 -0700

    parameterize get_upstream_branch.sh

--- get_upstream_branch.sh
+++ get_upstream_branch.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 # Define variables
-branch_name=$1
+dependency_name=$1
+branch_name=$2
 
-vivarium_branch_name='main'
+root_dir=$(pwd)
+dependency_branch_name='main'
 branch_name_to_check=${branch_name}
 iterations=0
 
-while [ "$branch_name_to_check" != "$vivarium_branch_name" ] && [ $iterations -lt 20 ]
+while [ "$branch_name_to_check" != "$dependency_branch_name" ] && [ $iterations -lt 20 ]
 do
-  echo "Checking for vivarium branch: '${branch_name_to_check}'"
+  echo "Checking for ${dependency_name} branch: '${branch_name_to_check}'"
   if
     git ls-remote --exit-code \
-    --heads https://github.com/ihmeuw/vivarium.git ${branch_name_to_check} == "0"
+    --heads https://github.com/ihmeuw/"${dependency_name}".git "${branch_name_to_check}" == "0"
   then
-    vivarium_branch_name=${branch_name_to_check}
-    echo "Found matching branch: ${vivarium_branch_name}"
+    dependency_branch_name=${branch_name_to_check}
+    echo "Found matching branch: ${dependency_branch_name}"
   else
     echo "Could not find upstream branch '${branch_name_to_check}'. Finding parent branch."
     branch_name_to_check="$( \
       git show-branch -a \
       | grep '\*' \
-      | grep -v `git rev-parse --abbrev-ref HEAD` \
+      | grep -v "${branch_name_to_check}" \
       | head -n1 \
       | sed 's/[^\[]*//' \
       | awk 'match($0, /\[[a-zA-Z0-9\/.-]+\]/) { print substr( $0, RSTART+1, RLENGTH-2 )}' \
@@ -32,9 +34,17 @@ do
       branch_name_to_check="main"
     fi
     echo "Checking out branch: ${branch_name_to_check}"
-    git checkout ${branch_name_to_check}
+    git checkout "${branch_name_to_check}"
     iterations=$((iterations+1))
   fi
   done
-  echo "vivarium_branch_name=${vivarium_branch_name}" >> $GITHUB_ENV
-  git checkout ${branch_name}
+  git checkout "${branch_name}"
+
+if [ "$dependency_branch_name" != "main" ]; then
+  echo "Cloning ${dependency_name} branch: ${dependency_branch_name}"
+  cd ..
+  git clone --branch="${dependency_branch_name}" https://github.com/ihmeuw/"${dependency_name}".git
+  cd "${dependency_name}" || exit
+  pip install .
+  cd "$root_dir" || exit
+fi
