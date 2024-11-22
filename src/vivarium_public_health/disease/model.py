ef5eac7398aa106095413f29ab2f5232ee10809e
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-11-21 19:39:37 -0800

    simplify initialization of DiseaseModel

--- src/vivarium_public_health/disease/model.py
+++ src/vivarium_public_health/disease/model.py
@@ -10,15 +10,15 @@ transitions at simulation initialization and during transitions.
 """
 import warnings
 from collections.abc import Callable, Iterable
+from functools import partial
 from typing import Any
 
-import numpy as np
 import pandas as pd
 from vivarium.exceptions import VivariumError
 from vivarium.framework.engine import Builder
-from vivarium.framework.event import Event
 from vivarium.framework.population import SimulantData
 from vivarium.framework.state_machine import Machine
+from vivarium.types import LookupTableData
 
 from vivarium_public_health.disease.state import BaseDiseaseState, SusceptibleState
 from vivarium_public_health.disease.transition import TransitionString
@@ -82,34 +82,11 @@ class DiseaseModel(Machine):
         cause_type: str = "cause",
         states: Iterable[BaseDiseaseState] = (),
         residual_state: BaseDiseaseState | None = None,
-        **kwargs,
     ):
-        super().__init__(cause, states=states, **kwargs)
+        super().__init__(cause, states=states)
         self.cause = cause
         self.cause_type = cause_type
-
-        if initial_state is not None:
-            warnings.warn(
-                "In the future, the 'initial_state' argument to DiseaseModel"
-                " will be used to initialize all simulants into that state. To"
-                " retain the current behavior of defining a residual state, use"
-                " the 'residual_state' argument.",
-                DeprecationWarning,
-                stacklevel=2,
-            )
-
-            if residual_state:
-                raise DiseaseModelError(
-                    "A DiseaseModel cannot be initialized with both"
-                    " 'initial_state and 'residual_state'."
-                )
-
-            self.residual_state = initial_state.state_id
-        elif residual_state is not None:
-            self.residual_state = residual_state.state_id
-        else:
-            self.residual_state = self._get_default_residual_state()
-
+        self.residual_state = self._get_residual_state(initial_state, residual_state)
         self._get_data_functions = (
             get_data_functions if get_data_functions is not None else {}
         )
@@ -120,12 +97,35 @@ class DiseaseModel(Machine):
 
         self.configuration_age_start = builder.configuration.population.initialization_age_min
         self.configuration_age_end = builder.configuration.population.initialization_age_max
+
         builder.value.register_value_modifier(
             "cause_specific_mortality_rate",
             self.adjust_cause_specific_mortality_rate,
             requires_columns=["age", "sex"],
         )
-        self.randomness = builder.randomness.get_stream(f"{self.state_column}_initial_states")
+
+    def on_initialize_simulants(self, pop_data: SimulantData) -> None:
+        """Initialize the simulants in the population.
+
+        If all simulants are initialized at age 0, birth prevalence is used.
+        Otherwise, prevalence is used.
+
+        Parameters
+        ----------
+        pop_data
+            The population data object.
+        """
+        if pop_data.user_data.get("age_end", self.configuration_age_end) == 0:
+            initialization_table_name = "birth_prevalence"
+        else:
+            initialization_table_name = "prevalence"
+
+        for state in self.states:
+            state.lookup_tables["initialization_weights"] = state.lookup_tables[
+                initialization_table_name
+            ]
+
+        super().on_initialize_simulants(pop_data)
 
     #################
     # Setup methods #
@@ -146,58 +146,6 @@ class DiseaseModel(Machine):
             )
         return csmr_data
 
-    ########################
-    # Event-driven methods #
-    ########################
-
-    def on_initialize_simulants(self, pop_data: SimulantData) -> None:
-        population = self.population_view.subview(["age", "sex"]).get(pop_data.index)
-
-        assert self.residual_state in {s.state_id for s in self.states}
-
-        if pop_data.user_data["sim_state"] == "setup":  # simulation start
-            if self.configuration_age_start != self.configuration_age_end != 0:
-                state_names, weights_bins = self.get_state_weights(
-                    pop_data.index, "prevalence"
-                )
-            else:
-                raise NotImplementedError(
-                    "We do not currently support an age 0 cohort. "
-                    "configuration.population.initialization_age_min and "
-                    "configuration.population.initialization_age_max "
-                    "cannot both be 0."
-                )
-
-        else:  # on time step
-            if pop_data.user_data["age_start"] == pop_data.user_data["age_end"] == 0:
-                state_names, weights_bins = self.get_state_weights(
-                    pop_data.index, "birth_prevalence"
-                )
-            else:
-                state_names, weights_bins = self.get_state_weights(
-                    pop_data.index, "prevalence"
-                )
-
-        if state_names and not population.empty:
-            # only do this if there are states in the model that supply prevalence data
-            population["sex_id"] = population.sex.apply({"Male": 1, "Female": 2}.get)
-
-            condition_column = self.assign_initial_status_to_simulants(
-                population,
-                state_names,
-                weights_bins,
-                self.randomness.get_draw(population.index),
-            )
-
-            condition_column = condition_column.rename(
-                columns={"condition_state": self.state_column}
-            )
-        else:
-            condition_column = pd.Series(
-                self.residual_state, index=population.index, name=self.state_column
-            )
-        self.population_view.update(condition_column)
-
     ##################################
     # Pipeline sources and modifiers #
     ##################################
@@ -209,40 +157,78 @@ class DiseaseModel(Machine):
     # Helper functions #
     ####################
 
-    def _get_default_residual_state(self):
-        susceptible_states = [s for s in self.states if isinstance(s, SusceptibleState)]
-        if len(susceptible_states) != 1:
-            raise DiseaseModelError("Disease model must have exactly one SusceptibleState.")
-        return susceptible_states[0].state_id
+    def _get_residual_state(
+        self, initial_state: BaseDiseaseState, residual_state: BaseDiseaseState
+    ) -> BaseDiseaseState:
+        """Get the residual state for the DiseaseModel.
 
-    def get_state_weights(
-        self, pop_index: pd.Index, prevalence_type: str
-    ) -> tuple[list[str], np.ndarray | None]:
-        states = [state for state in self.states if state.lookup_tables.get(prevalence_type)]
+        This will be the residual state if it is provided, otherwise it will be
+        the model's SusceptibleState. This method also calculates the residual
+        state's birth_prevalence and prevalence.
+        """
+        if initial_state is not None:
+            warnings.warn(
+                "In the future, the 'initial_state' argument to DiseaseModel"
+                " will be used to initialize all simulants into that state. To"
+                " retain the current behavior of defining a residual state, use"
+                " the 'residual_state' argument.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
 
-        if not states:
-            return states, None
+            if residual_state:
+                raise DiseaseModelError(
+                    "A DiseaseModel cannot be initialized with both"
+                    " 'initial_state and 'residual_state'."
+                )
 
-        weights = [state.lookup_tables.get(prevalence_type)(pop_index) for state in states]
-        for w in weights:
-            w.reset_index(inplace=True, drop=True)
-        weights += ((1 - np.sum(weights, axis=0)),)
+            residual_state = initial_state
+        elif residual_state is None:
+            susceptible_states = [s for s in self.states if isinstance(s, SusceptibleState)]
+            if len(susceptible_states) != 1:
+                raise DiseaseModelError(
+                    "Disease model must have exactly one SusceptibleState."
+                )
+            residual_state = susceptible_states[0]
 
-        weights = np.array(weights).T
-        weights_bins = np.cumsum(weights, axis=1)
+        if residual_state not in self.states:
+            raise DiseaseModelError(
+                f"Residual state '{self.residual_state}' must be one of the"
+                f" states: {self.states}."
+            )
 
-        state_names = [s.state_id for s in states] + [self.residual_state]
+        residual_state.birth_prevalence = partial(
+            self._get_residual_state_probabilities, table_name="birth_prevalence"
+        )
+        residual_state.prevalence = partial(
+            self._get_residual_state_probabilities, table_name="prevalence"
+        )
 
-        return state_names, weights_bins
+        return residual_state
 
-    @staticmethod
-    def assign_initial_status_to_simulants(
-        simulants_df, state_names, weights_bins, propensities
-    ):
-        simulants = simulants_df[["age", "sex"]].copy()
+    def _get_residual_state_probabilities(
+        self, builder: Builder, table_name: str
+    ) -> LookupTableData:
+        """Calculate the probabilities of the residual state based on the other states."""
+        non_residual_states = [s for s in self.states if s != self.residual_state]
+        non_residual_probabilities = 0
+        for state in non_residual_states:
+            weights_source = builder.configuration[state.name].data_sources[table_name]
+            weights = state.get_data(builder, weights_source)
+            if isinstance(weights, pd.DataFrame):
+                weights = weights.set_index(
+                    [c for c in weights.columns if c != "value"]
+                ).squeeze()
+            non_residual_probabilities += weights
 
-        choice_index = (propensities.values[np.newaxis].T > weights_bins).sum(axis=1)
-        initial_states = pd.Series(np.array(state_names)[choice_index], index=simulants.index)
+        residual_probabilities = 1 - non_residual_probabilities
 
-        simulants.loc[:, "condition_state"] = initial_states
-        return simulants
+        if pd.Series(residual_probabilities < 0).any():
+            raise ValueError(
+                f"The {table_name} for the states in the DiseaseModel must sum"
+                " to less than 1."
+            )
+        if isinstance(residual_probabilities, pd.Series):
+            residual_probabilities = residual_probabilities.reset_index()
+
+        return residual_probabilities
