0efd3d6f5b7132ef485a20c2ea6026918d0e0074
Jim Albright <albrja@uw.edu>
2025-07-09 15:15:15 -0700

    Add exposre to init

--- src/vivarium_public_health/risks/__init__.py
+++ src/vivarium_public_health/risks/__init__.py
@@ -1,5 +1,6 @@
 from .base_risk import Risk
 from .effect import InterventionEffect, NonLogLinearRiskEffect, RiskEffect
+from .exposure import Exposure
 from .implementations.low_birth_weight_and_short_gestation import (
     LBWSGDistribution,
     LBWSGRisk,
