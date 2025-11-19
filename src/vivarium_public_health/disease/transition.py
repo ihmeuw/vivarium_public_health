7fe9948576158db3149232d3507a916b72044b64
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2025-11-19 11:23:29 -0700

    Sbachmei/mic 6618/update disease package (#552)

--- src/vivarium_public_health/disease/transition.py
+++ src/vivarium_public_health/disease/transition.py
@@ -53,10 +53,6 @@ class RateTransition(Transition):
         }
 
     @property
-    def columns_required(self) -> list[str] | None:
-        return ["alive"]
-
-    @property
     def transition_rate_pipeline_name(self) -> str:
         if self._get_data_functions:
             if "incidence_rate" in self._get_data_functions:
@@ -175,7 +171,9 @@ class RateTransition(Transition):
 
     def compute_transition_rate(self, index: pd.Index) -> pd.Series:
         transition_rate = pd.Series(0.0, index=index)
-        living = self.population_view.get(index, query='alive == "alive"').index
+        living = self.population_view.get_private_columns(
+            index, query_columns=["alive"], query='alive == "alive"'
+        ).index
         base_rates = self.lookup_tables["transition_rate"](living)
         joint_paf = self.joint_paf(living)
         transition_rate.loc[living] = base_rates * (1 - joint_paf)
