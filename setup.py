586a7c09f94f090c9cf497be72f32b26a3930154
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2025-12-26 19:57:52 -0500

    update vph to work with changes to how lookup tables are built (#572)
    * make exposure distribution ppf method a pipeline
    
    * make lbwsg exposure distribution ppf a pipeline
    
    * simplify setting initialization weights lookup tables

--- setup.py
+++ setup.py
@@ -44,7 +44,9 @@ if __name__ == "__main__":
     install_requirements = [
         "vivarium_dependencies[pandas,numpy_lt_2,scipy,tables,loguru,pyarrow]",
         "vivarium_build_utils>=2.0.1,<3.0.0",
-        "vivarium>=4.0.0,<5.0.0",
+        # FIXME: Uncomment once Vivarium 4.1.0 is released
+        # "vivarium>=4.1.0",
+        "vivarium @ git+https://github.com/ihmeuw/vivarium.git@release_candidate/v4.1.0",
         "layered_config_tree",
         "risk_distributions>=2.0.11",
     ]
