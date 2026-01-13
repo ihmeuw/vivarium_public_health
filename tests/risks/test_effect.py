b05978f8c7f2f396298b58c87c754ec82c6095af
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2026-01-12 17:06:20 -0700

    Sbachmei/replace list attributes calls with get columns (#582)

--- tests/risks/test_effect.py
+++ tests/risks/test_effect.py
@@ -636,7 +636,7 @@ def test_relative_risk_pipeline(dichotomous_risk, base_config, base_plugins):
     pop_idx = sim.get_population_index()
 
     expected_pipeline = f"{effect.risk.name}_on_{effect.target.name}.relative_risk"
-    assert expected_pipeline in sim.list_attributes()
+    assert expected_pipeline in sim.get_columns()
 
     rr_mapper = {
         "cat1": 1.4,
