55570ab3182141977b90a26fe9c9aeff5a48c88c
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2026-01-20 20:44:59 -0500

    set default allow self transitions to True (#588)

--- tests/disease/test_disease.py
+++ tests/disease/test_disease.py
@@ -52,7 +52,6 @@ def test_dwell_time(base_config, base_plugins, disease):
         prevalence=0.0,
         dwell_time=pd.Timedelta(days=28),
         disability_weight=0.0,
-        allow_self_transition=True,
     )
     done_state = BaseDiseaseState("sick")
 
@@ -107,7 +106,7 @@ def test_dwell_time_with_mortality(base_config, base_plugins, disease):
         "disability_weight": 0.0,
     }
 
-    mortality_state = DiseaseState("event", **mortality_kwargs, allow_self_transition=True)
+    mortality_state = DiseaseState("event", **mortality_kwargs)
     done_state = BaseDiseaseState("sick")
 
     healthy_state.add_dwell_time_transition(mortality_state)
@@ -165,7 +164,6 @@ def test_prevalence_single_state_with_migration(
         prevalence=test_prevalence_level,
         disability_weight=0.0,
         dwell_time=pd.Timedelta(days=1),
-        allow_self_transition=True,
     )
     model = DiseaseModel(disease, residual_state=healthy, states=[healthy, sick])
     base_config.update({"population": {"population_size": 50000}}, **metadata(__file__))
@@ -206,7 +204,6 @@ def test_prevalence_multiple_sequelae(
             prevalence=p,
             disability_weight=0.0,
             dwell_time=pd.Timedelta(days=1),
-            allow_self_transition=True,
         )
         for i, p in enumerate(test_prevalence_level)
     }
